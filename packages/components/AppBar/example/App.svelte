abfae1fef6e55f7fdbdfd246344cd82ea5581ff5
Christian Kaisermann <christian@kaisermann.me>
2018-05-14 19:36:20 -0300

    Add partial @mamba/appbar

---
+++ packages/components/AppBar/example/App.svelte
@@ -0,0 +1,39 @@
+<h1>Default AppBar</h1>
+<br><br>
+<AppBar></AppBar>
+
+<h1>Colored AppBar</h1>
+<br><br>
+<AppBar color="red"></AppBar>
+
+<h1>AppBar with title</h1>
+<br><br>
+<AppBar title="Title"></AppBar>
+
+<h1>AppBar with left icon</h1>
+<br><br>
+<AppBar leftIcon="X"></AppBar>
+
+<h1>AppBar with right icon</h1>
+<br><br>
+<AppBar rightIcon="X"></AppBar>
+
+<h1>AppBar with left icon and title</h1>
+<br><br>
+<AppBar leftIcon="X" title="Title"></AppBar>
+
+<h1>AppBar with left icon and title</h1>
+<br><br>
+<AppBar rightIcon="X" title="Title"></AppBar>
+
+<h1>AppBar with left/right icon and title</h1>
+<br><br>
+<AppBar leftIcon="X" rightIcon="X" title="Title"></AppBar>
+
+<script>
+  export default {
+    components: {
+      AppBar: '../src',
+    },
+  }
+</script>
