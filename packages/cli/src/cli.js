7bcec25b2661371d3095169f10eb6cce8a4b53b2
Christian Kaisermann <christian@kaisermann.me>
2018-05-21 19:25:52 -0300

    Use yargs in @mamba/cli

--- packages/cli/src/cli.js
+++ packages/cli/src/cli.js
@@ -2,47 +2,71 @@
 
 const { fromCwd } = require('quickenv')
 const shell = require('shelljs')
-const args = require('argv').run()
+const yargs = require('yargs')
 const getManifest = require('./getManifest')
 const { slugify } = require('./utils')
 
 const DIST_PATH = fromCwd('dist')
-const MAINAPP_PATH = 'data/app/MAINAPP'
-const REMOTE_ADDRESS = 'POS'
-const REMOTE_APPS_DIR = `${REMOTE_ADDRESS}:/${MAINAPP_PATH}/apps/`
-
-const [cmd] = args.targets
+const REMOTE_HOST = 'POS'
+const REMOTE_MAINAPP_DIR = 'data/app/MAINAPP'
+const REMOTE_APPS_DIR = `${REMOTE_HOST}:/${REMOTE_MAINAPP_DIR}/apps/`
 
 const execRemote = (...cmdList) => {
-  shell.exec(`ssh POS 'cd ${MAINAPP_PATH}; ${cmdList.join(';')}'`)
+  shell.exec(`ssh POS 'cd ${REMOTE_MAINAPP_DIR}; ${cmdList.join(';')}'`)
 }
 
 const CMDS = {
   stop: 'killall StoneMambaLoader StoneMambaSystem',
-  start: './StoneMambaLoader &',
+  start: './StoneMambaLoader  </dev/null &>/dev/null &',
 }
 
-switch (cmd) {
-  case 'deploy':
-    const { displayedName: appName, id: appID } = getManifest()
-    const appSlug = `${appID}-${slugify(appName)}`
-    shell.exec(
-      `rsync -zzaP --delete ${DIST_PATH}/ ${REMOTE_APPS_DIR}/${appSlug}.stone`,
-    )
-    break
-
-  case 'stop':
-    execRemote(CMDS.stop)
-    break
-
-  case 'start':
-    execRemote(CMDS.start)
-    break
-
-  case 'restart':
-    execRemote(CMDS.stop, CMDS.start)
-    break
-
-  default:
-    throw new Error(`Invalid command "${cmd}"`)
+const checkCommands = (yargs, min) => {
+  if (yargs.argv._.length < min) {
+    yargs.showHelp()
+  }
 }
+
+yargs
+  .usage('usage: $0 <command>')
+  .command('app', 'Apps related functionalities', yargs => {
+    yargs.command('deploy', 'deploy the current app', () => {
+      const { displayedName: appName, id: appID } = getManifest()
+      const appSlug = `${appID}-${slugify(appName)}`
+      shell.exec(
+        `rsync -zzaP --delete ${DIST_PATH}/ ${REMOTE_APPS_DIR}/${appSlug}.stone`,
+        {
+          silent: true,
+        },
+      )
+      console.log('App deployed')
+    })
+    checkCommands(yargs, 2)
+  })
+  .command('pos', 'POS related functionalities', yargs => {
+    yargs
+      .usage('usage: mamba pos <command>')
+      .command('ssh-init', 'initializes the POS ssh server', () => {
+        shell.exec('start_ssh.sh com:/dev/ttyPos0')
+      })
+      .command('connect', 'connect to the POS via ssh', () => {
+        shell.exec(
+          `ssh -tt ${REMOTE_HOST} 'cd ${REMOTE_MAINAPP_DIR}; /bin/sh -l'`,
+        )
+      })
+      .command('stop', 'stop the Mamba OS', () => {
+        console.info('Stopping Mamba OS')
+        execRemote(CMDS.stop)
+      })
+      .command('start', 'start the Mamba OS', () => {
+        console.info('Starting Mamba OS')
+        execRemote(CMDS.start)
+      })
+      .command('restart', 'restart the Mamba OS', () => {
+        console.info('Restarting Mamba OS')
+        execRemote(CMDS.stop, CMDS.start)
+      })
+    checkCommands(yargs, 2)
+  })
+  .strict()
+  .help()
+  .parse()
