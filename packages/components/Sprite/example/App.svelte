b5dcde50a7be3be0f45f13a1bfebb6ba53b0f871
Christian Kaisermann <christian@kaisermann.me>
2018-05-30 18:31:08 -0300

    Add @mamba/sprite

---
+++ packages/components/Sprite/example/App.svelte
@@ -0,0 +1,42 @@
+<div class="container">
+  <h1>Default sprite <span>{'<Sprite src="..." width="..." height="..."/>'}</span></h1>
+  <div class="row">
+    <Sprite ref:sprite src="./static/stone.png" width="70px"/>
+  </div>
+  <button on:click="refs.sprite.start()">play</button>
+  <button on:click="refs.sprite.stop()">pause</button>
+</div>
+
+<script>
+
+  export default {
+    components: {
+      Sprite: '../src',
+    },
+  }
+</script>
+
+<style>
+  :global(body) {
+    background-color: #ddd;
+  }
+
+  h1 span {
+    display: inline-block;
+    vertical-align: middle;
+    margin-left: 10px;
+    font-size: 14px;
+    font-weight: bold;
+    font-family: monospace;
+  }
+
+  .container {
+    max-width: 80%;
+    width: 700px;
+    margin: 80px auto;
+  }
+
+  .row {
+    margin-bottom: 50px
+  }
+</style>
---
+++ packages/components/Sprite/src/Sprite.svelte
@@ -0,0 +1,57 @@
+<div class="sprite" ref:sprite style="background-position: {position}px 0"></div>
+
+<script>
+  const FPS = 60
+
+  export default {
+    data() {
+      return {
+        spriteWidth: null,
+        position: 0,
+      }
+    },
+    oncreate() {
+      const { src, width, height } = this.options.data
+      const { sprite } = this.refs
+      const image = new Image()
+
+      sprite.style.backgroundImage = `url(${src})`
+      sprite.style.width = width
+      sprite.style.height = height || width
+
+      image.src = src
+      image.onload = () => {
+        this.set({ spriteWidth: image.width })
+        this.start()
+      }
+    },
+    methods: {
+      start() {
+        const { spriteWidth, position } = this.get()
+        const STEP = spriteWidth ? spriteWidth / FPS : 0
+        let positionAux = position
+
+        const interval = setInterval(() => {
+          positionAux -= STEP
+          if (positionAux < -spriteWidth) {
+            positionAux = 0
+          }
+          this.set({ position: positionAux })
+        }, 1000 / FPS)
+
+        this.set({ interval })
+      },
+      stop() {
+        const { interval } = this.get()
+        clearInterval(interval)
+        this.set({ interval: null })
+      },
+    },
+  }
+</script>
+
+<style>
+  .sprite {
+    display: inline-block
+  }
+</style>
