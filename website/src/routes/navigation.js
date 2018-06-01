7808d910b31a7bac3f52528f11f981449adbff05
Jaime C Marques <jaime.mqu@gmail.com>
2018-06-01 16:44:44 -0300

    Create navigation object

--- website/src/routes/navigation.js
+++ website/src/routes/navigation.js
@@ -1,7 +1,24 @@
 export default [
-  { title: 'Intro', to: '/intro' },
+  { title: 'Getting Started', to: '/home' },
   {
-    title: 'Elements',
-    submenus: [{ title: 'Icon', to: '/components/icon' }],
+    title: 'Native API',
+    to: '/native',
+    submenu: [{ title: 'Quickstart', to: '/native/quickstart' }],
   },
+  { title: 'Styles', to: '/styles' },
+  {
+    title: 'Components',
+    to: '/components',
+    submenu: [
+      { title: 'App', to: '/app' },
+      { title: 'AppBar', to: '/appbar' },
+      { title: 'Collection', to: '/collection' },
+      { title: 'Dialog', to: '/dialog' },
+      { title: 'Icon', to: '/icon' },
+      { title: 'POS', to: '/pos' },
+      { title: 'Switch', to: '/switch' },
+    ],
+  },
+  { title: 'Utils', to: '/utils' },
+  { title: 'Development', to: '/development' },
 ]
