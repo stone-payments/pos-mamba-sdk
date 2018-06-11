c41013bff5d35da4ae01170188cdefb3d6c73461
Christian Kaisermann <christian@kaisermann.me>
2018-06-11 15:36:43 -0300

    Add utility keyboard methods to @mamba/native/keyboard

--- packages/native/src/keyboard/shared.js
+++ packages/native/src/keyboard/shared.js
@@ -6,7 +6,31 @@ const disableBackspace = event => {
   }
 }
 
+const KEYMAP = Object.freeze({
+  13: 'enter',
+  8: 'back',
+  27: 'close',
+  17: 'help',
+  16: 'shortcuts',
+  48: '0',
+  49: '1',
+  50: '2',
+  51: '3',
+  52: '4',
+  53: '5',
+  54: '6',
+  55: '7',
+  56: '8',
+  57: '9',
+})
+
 export default function(Keyboard) {
+  Keyboard.getKeyCode = keyName =>
+    Object.keys(KEYMAP).find(code => KEYMAP[code] === keyName)
+  Keyboard.getKeyName = keyCode => KEYMAP[keyCode]
+  Keyboard.isNumericKey = keyCode => !isNaN(parseFloat(KEYMAP[keyCode]))
+  Keyboard.isActionKey = keyCode => !Keyboard.isNumericKey(keyCode)
+
   Keyboard.disableBackspace = () => {
     if (isBackspaceEnabled) {
       document.addEventListener('keydown', disableBackspace)
@@ -20,22 +44,4 @@ export default function(Keyboard) {
       isBackspaceEnabled = true
     }
   }
-
-  Keyboard.KEYMAP = Object.freeze({
-    13: 'enter',
-    8: 'back',
-    27: 'close',
-    17: 'help',
-    16: 'shortcuts',
-    48: '0',
-    49: '1',
-    50: '2',
-    51: '3',
-    52: '4',
-    53: '5',
-    54: '6',
-    55: '7',
-    56: '8',
-    57: '9',
-  })
 }
