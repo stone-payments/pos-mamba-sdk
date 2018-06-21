418f38b9d2faf03dd5ed6e16a097d86014a1e1ff
Christian Kaisermann <christian@kaisermann.me>
2018-06-21 19:46:35 -0300

    Add support for // comments

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,14 @@
 module.exports = {
+  syntax: 'postcss-scss',
   plugins: [
     require('postcss-easy-import'),
-    require('postcss-advanced-variables'),
-    require('postcss-atroot'),
     require('postcss-extend-rule'),
-    require('postcss-nested'),
+    require('postcss-advanced-variables'),
     require('postcss-preset-env')({
       stage: 2 /** Defaults postcss-preset-env to stage 2 */,
     }),
+    require('postcss-atroot'),
     require('postcss-property-lookup'),
+    require('postcss-nested'),
   ],
 }
