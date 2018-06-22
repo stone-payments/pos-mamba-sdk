6bb4f4fc4c81819bd9881570bfc4a4346aabaf05
Christian Kaisermann <christian@kaisermann.me>
2018-06-22 17:06:07 -0300

    Modularize @mamba/pos and add status bar

--- packages/components/Pos/src/Pos.svelte
+++ packages/components/Pos/src/Pos.svelte
@@ -3,27 +3,10 @@
 <div class="wrapper">
   <div class="shadow"></div>
   <div class="pos">
-    <div class="brightness" style="opacity: {brightnessOpacity};"></div>
-    <div class="screen">
+    <Screen>
       <slot></slot>
-    </div>
-    <div class="actions">
-      <button class="key-1" on:mousedown="dispatchKey({ event, keyName: '1'})">1</button>
-      <button class="key-2" on:mousedown="dispatchKey({ event, keyName: '2'})">2</button>
-      <button class="key-3" on:mousedown="dispatchKey({ event, keyName: '3'})">3</button>
-      <button class="key-4" on:mousedown="dispatchKey({ event, keyName: '4'})">4</button>
-      <button class="key-5" on:mousedown="dispatchKey({ event, keyName: '5'})">5</button>
-      <button class="key-6" on:mousedown="dispatchKey({ event, keyName: '6'})">6</button>
-      <button class="key-7" on:mousedown="dispatchKey({ event, keyName: '7'})">7</button>
-      <button class="key-8" on:mousedown="dispatchKey({ event, keyName: '8'})">8</button>
-      <button class="key-9" on:mousedown="dispatchKey({ event, keyName: '9'})">9</button>
-      <button class="key-0" on:mousedown="dispatchKey({ event, keyName: '0'})">0</button>
-      <button class="back" on:mousedown="dispatchKey({ event, keyName: 'back'})">back</button>
-      <button class="close" on:mousedown="dispatchKey({ event, keyName: 'close'})">close</button>
-      <button class="enter" on:mousedown="dispatchKey({ event, keyName: 'enter'})">enter</button>
-      <button class="shortcuts" on:mousedown="dispatchKey({ event, keyName: 'shortcuts'})">shortcuts</button>
-      <button class="help" on:mousedown="dispatchKey({ event, keyName: 'help'})">help</button>
-    </div>
+    </Screen>
+    <Keypad />
   </div>
 </div>
 
@@ -31,21 +14,9 @@
   import Keyboard from '@mamba/native/keyboard'
 
   export default {
-    data() {
-      return {
-        brightnessLevel: 10,
-      }
-    },
-    computed: {
-      brightnessOpacity: ({ brightnessLevel }) => 1 - brightnessLevel / 10,
-    },
-    oncreate() {
-      if (this.store) {
-        /** Listen for brightness changes */
-        this.store.on('change:brightness', ({ brightnessLevel }) => {
-          this.set({ brightnessLevel })
-        })
-      }
+    components: {
+      Keypad: './Keypad.svelte',
+      Screen: './Screen.svelte',
     },
     methods: {
       /** Treat backspace as the 'back button' */
@@ -68,50 +39,11 @@
           ) // eslint-disable-line
         }
       },
-      dispatchKey({ event, keyName }) {
-        /** Prevent the button from being focused */
-        event.preventDefault()
-        const focusedEl = document.activeElement
-        const isFocusedInput = focusedEl && focusedEl.tagName === 'INPUT'
-
-        const code = Keyboard.getKeyCode(keyName)
-
-        /** If action button clicked */
-        if (Keyboard.isActionKey(code)) {
-          /** The actual 'back' is handled by that 'handleKeyUp' method */
-          if (keyName === 'back') {
-            /** If we're focusing on a <input> erase the last character */
-            if (isFocusedInput) {
-              focusedEl.value = focusedEl.value.slice(0, -1)
-              focusedEl.dispatchEvent(new Event('input'))
-            }
-          }
-        } else {
-          /** If numeric button clicked */
-          if (isFocusedInput) {
-            focusedEl.value += keyName
-            focusedEl.dispatchEvent(new Event('input'))
-          }
-        }
-
-        window.dispatchEvent(
-          new KeyboardEvent('keyup', {
-            key: keyName,
-            keyCode: code,
-          }),
-        )
-      },
     },
   }
 </script>
 
 <style>
-  @media all and (max-width: 480px) {
-    .actions {
-      display: none;
-    }
-  }
-
   @media all and (min-width: 481px) {
     .wrapper {
       display: flex;
@@ -145,7 +77,7 @@
         z-index: 0;
         filter: brightness(0) blur(2px);
         opacity: 0.4;
-        animation: shadow .8s ease-out forwards;
+        animation: shadow 0.8s ease-out forwards;
       }
 
       @keyframes shadow {
@@ -156,127 +88,6 @@
           transform: translate(15px, 15px);
         }
       }
-    }
-
-    .screen,
-    .brightness {
-      position: absolute;
-      top: 231px;
-      left: 50px;
-      width: 240px;
-      height: 320px;
-    }
-
-    .screen {
-      background-color: #fff;
-      overflow-x: hidden;
-      overflow-y: auto;
-    }
-
-    .screen::-webkit-scrollbar {
-      display: none;
-    }
-
-    .brightness {
-      background-color: #000;
-      z-index: 100000;
-      pointer-events: none;
-      transition: opacity 0.3s ease;
-    }
-
-    /**
-       * Override the position: fixed to display content in the POS mockup.
-       * This may eventually brake.
-       */
-    .screen :global(.button.is-fixed),
-    .screen :global(.dialog) {
-      position: absolute !important;
-    }
-
-    button {
-      position: absolute;
-      width: 60px;
-      height: 30px;
-      border: 0;
-      -webkit-appearance: none;
-      cursor: pointer;
-      background: none;
-      border-radius: 8px;
-      color: transparent;
-      font-weight: bold;
-      font-size: 18px;
-    }
-
-    button:hover {
-      opacity: 1;
-      color: white;
-      background-color: rgba(22, 22, 22, 0.8);
-      border: 2px solid white;
-    }
-
-    .shortcuts {
-      font-size: 10px;
-    }
-
-    .close {
-      right: 50px;
-    }
-
-    .back {
-      bottom: 101px;
-      right: 50px;
-    }
-
-    .enter {
-      height: 64px;
-      right: 50px;
-    }
-
-    .key-1,
-    .key-4,
-    .key-7,
-    .help {
-      right: 255px;
-    }
-
-    .key-2,
-    .key-5,
-    .key-8,
-    .key-0 {
-      right: 187px;
-    }
-
-    .key-3,
-    .key-6,
-    .key-9,
-    .shortcuts {
-      right: 119px;
-    }
-
-    .key-1,
-    .key-2,
-    .key-3,
-    .close {
-      bottom: 138px;
-    }
-
-    .key-4,
-    .key-5,
-    .key-6 {
-      bottom: 102px;
-    }
-
-    .key-7,
-    .key-8,
-    .key-9 {
-      bottom: 67px;
-    }
-
-    .key-0,
-    .shortcuts,
-    .help,
-    .enter {
-      bottom: 32px;
     }
   }
 </style>
---
+++ packages/components/Pos/src/Screen.svelte
@@ -0,0 +1,111 @@
+<div class="screen">
+  <div class="brightness" style="opacity: {brightnessOpacity};"></div>
+  <div class="status-bar">
+    <div>
+      <div class="version">{version}</div>
+    </div>
+    <div>
+      <div class="version">{time}</div>
+    </div>
+  </div>
+  <div class="content">
+    <slot></slot>
+  </div>
+</div>
+
+<script>
+  export default {
+    data() {
+      return {
+        version: '2.1.0',
+        brightnessLevel: 10,
+        time: '00:00',
+      }
+    },
+    computed: {
+      brightnessOpacity: ({ brightnessLevel }) => 1 - brightnessLevel / 10,
+    },
+    oncreate() {
+      /** Update the clock after each second */
+      const updateTimer = () => {
+        const curDate = new Date()
+        const minutes = String(curDate.getMinutes()).padStart(2, '0')
+        const hours = curDate.getHours()
+        this.set({ time: `${hours}:${minutes}` })
+        return updateTimer
+      }
+      setInterval(updateTimer(), 1000)
+
+      if (this.store) {
+        /** Listen for brightness changes */
+        this.store.on('change:brightness', ({ brightnessLevel }) => {
+          this.set({ brightnessLevel })
+        })
+      }
+    },
+  }
+</script>
+
+<style>
+  .status-bar {
+    display: none;
+  }
+
+  @media all and (min-width: 481px) {
+    .screen {
+      position: absolute;
+      top: 231px;
+      left: 50px;
+      width: 240px;
+      height: 320px;
+      display: flex;
+      flex-flow: column;
+      overflow: hidden;
+    }
+
+    .content {
+      height: 100%;
+      background-color: #fff;
+      overflow-x: hidden;
+      overflow-y: auto;
+      flex: 0 1 auto;
+    }
+
+    .content::-webkit-scrollbar {
+      display: none;
+    }
+
+    .brightness {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: #000;
+      z-index: 100000;
+      pointer-events: none;
+      transition: opacity 0.3s ease;
+    }
+
+    .status-bar {
+      position: relative;
+      z-index: 1002;
+      width: 100%;
+      height: 20px;
+      flex: 0 0 20px;
+      background-color: #000;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0 4px;
+      color: #fff;
+      font-size: 12px;
+      font-weight: bold;
+    }
+
+    .screen :global(.button.is-fixed),
+    .screen :global(.dialog) {
+      position: absolute !important;
+    }
+  }
+</style>
