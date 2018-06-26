90abb06c1d125c8e481567e573c8845dd7c669b7
Christian Kaisermann <christian@kaisermann.me>
2018-06-26 17:05:32 -0300

    Remove camelcase rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,5 @@
 const { IS_PROD } = require('quickenv')
 
-const rules = {
-  indent: ['error', 2, { SwitchCase: 1 }],
-  'no-console': IS_PROD() ? ['error', { allow: ['warn', 'error'] }] : 'off',
-  'no-var': 'error',
-  'comma-dangle': ['error', 'always-multiline'],
-  'no-new': 'off',
-  camelcase: [
-    'error',
-    {
-      ignoreDestructuring: true,
-    },
-  ],
-}
-
 module.exports = {
   extends: [
     'standard',
@@ -32,21 +18,20 @@ module.exports = {
     es6: true,
   },
   parserOptions: {
-    ecmaFeatures: {
-      jsx: false,
-      modules: true,
-    },
+    ecmaVersion: 2018,
   },
-  rules: rules,
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    camelcase: 'off',
+    'no-console': IS_PROD() ? ['error', { allow: ['warn', 'error'] }] : 'off',
+    'no-var': 'error',
+    'comma-dangle': ['error', 'always-multiline'],
+    'no-new': 'off',
+  },
   overrides: [
     {
       files: ['**/__tests__/**/*.js', '**/*.test.js'],
       env: { jest: true },
-    },
-    /** Allow native to use console methods */
-    {
-      files: ['packages/native/**/*.js'],
-      rules: Object.assign({}, rules, { 'no-console': 0 }),
     },
   ],
 }
