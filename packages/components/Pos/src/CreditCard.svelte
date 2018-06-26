9a072117ddf0681043eb5ed6f2e7cd7916a2ad54
pedroglp <pedrogabriellancelloti@poli.ufrj.br>
2018-06-26 10:11:43 -0300

    adicionando arquivo estático e svelte do cartão

---
+++ packages/components/Pos/src/CreditCard.svelte
@@ -0,0 +1,49 @@
+<div class="credit-card inserted" on:click="toggleCard()">
+</div>
+
+<script>
+  export default {
+    data() {
+      return {
+        inserted: true,
+      }
+    },
+    methods: {
+      toggleCard(){
+        this.inserted = !this.inserted;
+        console.log(this.inserted)
+        if(this.inserted)
+          this.class = 'credit-card inserted';
+        else
+          this.class = 'credit-card';
+      },
+    },
+    oncreate() {
+    },
+  }
+</script>
+
+<style>
+  .credit-card {
+    display: none;
+  }
+
+  @media all and (min-width: 481px) {
+    .credit-card {
+      display: block;
+      position: relative;
+      width: 240px;
+      height: 370px;
+      top: 770px;
+      left: 40px;
+      z-index: 2;
+      background-image: url('./assets/creditcard.png');
+    }
+    .inserted {
+      display: block;
+      position: relative;
+      top:720px;
+      z-index: 2;
+    }
+  }
+</style>
