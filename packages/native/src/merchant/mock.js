9199a80666eeefe8d882d36674fd07e9080245eb
pedroglp <pedrogabriellancelloti@poli.ufrj.br>
2018-07-05 17:46:54 -0300

    fix typo in JSDoc

--- packages/native/src/merchant/mock.js
+++ packages/native/src/merchant/mock.js
@@ -2,11 +2,12 @@ export default function(Merchant) {
   const SimulatedInfo = {
     acquirerIssuedMerchantId: '123', // stone code
   }
+  /**
+   * Get the stone code
+   * @memberof Merchant
+   * @return {string} The stone code
+   */
 
-  /* Get the stone code
- * @memberof Merchant
- * @return {string} The stone code
- */
   function getStoneCode() {
     return SimulatedInfo.acquirerIssuedMerchantId
   }
