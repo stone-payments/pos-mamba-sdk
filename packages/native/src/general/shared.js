20f7ce5907050bf61874cbdb512a3592d055965f
Christian Kaisermann <christian@kaisermann.me>
2018-07-09 18:07:24 -0300

    Remove code related to the settings app

--- packages/native/src/general/shared.js
+++
@@ -1,9 +0,0 @@
-import SignalHandler from '../SignalHandler'
-
-export default function(General) {
-  const GeneralSignals = SignalHandler(General)
-
-  GeneralSignals.on('onFactoryResetEnd', () => {
-    General.reboot()
-  })
-}
