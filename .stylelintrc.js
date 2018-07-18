371a73ffd06cb1dc5d69d1abf30ac17d0e066ca2
Christian Kaisermann <christian@kaisermann.me>
2018-07-18 16:00:17 -0300

    New stylelint config for svelte. Lint all files

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,18 +1,3 @@
 module.exports = {
-  extends: 'stylelint-config-standard',
-  rules: {
-    'no-empty-source': null,
-    'value-list-comma-newline-after': null,
-    'declaration-colon-newline-after': null,
-    'at-rule-no-unknown': null,
-    /** Allow 'ref:*' selectors */
-    'selector-type-no-unknown': null,
-    'selector-pseudo-class-no-unknown': null,
-    /**
-     * Since stylelint reads all 'ref:*' as the same selector,
-     * this prevents it from complaining when multiple 'ref:*'
-     * are present on the same css
-     */
-    'no-descending-specificity': null,
-  },
+  extends: './packages/stylelint-config/index.js',
 };
