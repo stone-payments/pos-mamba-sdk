727cae4a41f1899ba6c558055370fd2ffbc99b90
pedroglp <pedrogabriellancelloti@poli.ufrj.br>
2018-07-19 10:56:19 -0300

    fixing signal name

--- packages/native/src/http/native.js
+++ packages/native/src/http/native.js
@@ -3,13 +3,15 @@ import SignalHandler from '../signal/handler.js'
 export default function(Http) {
   const HttpSignals = SignalHandler(Http)
 
+  console.log(`Native: ${Http}`)
   Http.send = function(configParams) {
+    /** Call asynchronus send request at the backend */
+    Http.doSend(configParams)
     return new Promise((resolve, reject) => {
-      HttpSignals.once('sendDoneCallback', () => {
+      console.log(`Promise: ${Http}`)
+      HttpSignals.once('requestFinished', () => {
         Http.getError() ? reject(Http.getError()) : resolve(Http.getData())
       })
-      /** Call asynchronus send request at the backend */
-      Http.doSend(configParams)
     })
   }
 }
