b9ccf207129191a66f5d4c2110e1df44bdf16d14
pedroglp <pedrogabriellancelloti@poli.ufrj.br>
2018-07-19 11:48:07 -0300

    removing console logs, moving http.doSend after handler

--- packages/native/src/http/native.js
+++ packages/native/src/http/native.js
@@ -2,16 +2,13 @@ import SignalHandler from '../signal/handler.js'
 
 export default function(Http) {
   const HttpSignals = SignalHandler(Http)
-
-  console.log(`Native: ${Http}`)
   Http.send = function(configParams) {
-    /** Call asynchronus send request at the backend */
-    Http.doSend(configParams)
     return new Promise((resolve, reject) => {
-      console.log(`Promise: ${Http}`)
       HttpSignals.once('requestFinished', () => {
         Http.getError() ? reject(Http.getError()) : resolve(Http.getData())
       })
+      /** Call asynchronus send request at the backend */
+      Http.doSend(configParams)
     })
   }
 }
