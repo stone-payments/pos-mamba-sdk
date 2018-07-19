7ae25d84d5ac79c9388137827dd22be7d9f01fac
Christian Kaisermann <christian@kaisermann.me>
2018-07-19 14:42:20 -0300

    Add develop methods to @mamba/native/app.js

--- packages/native/src/app/mock.js
+++ packages/native/src/app/mock.js
@@ -11,10 +11,44 @@ function isRunningOnDevice() {
   return false;
 }
 
+function listApps() {
+  return [
+    {
+      id: 1,
+      name: 'app1',
+      icon: '',
+    },
+    {
+      id: 2,
+      name: 'app2',
+      icon: '',
+    },
+  ];
+}
+
+function downloadAndSave(tar, destination) {
+  console.log(`downloading tar: ${tar}`);
+  console.log(`saving in: ${destination}`);
+}
+
+function installApp(tarPath, destination) {
+  console.log(`tarPath: ${tarPath}`);
+  console.log(`destination: ${destination}`);
+}
+
+function deleteApp(appId) {
+  console.log(`appId: ${appId}`);
+}
+
 export default function (App) {
   Object.assign(App, {
     doClose,
     getAppKey,
     isRunningOnDevice,
+    // TODO: Refactor exclusive 'develop' app methods
+    listApps,
+    downloadAndSave,
+    installApp,
+    deleteApp,
   });
 }
