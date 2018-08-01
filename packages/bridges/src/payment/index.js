daadf79f3e93dd9500849877446c90232ad75dca
Christian Kaisermann <christian@kaisermann.me>
2018-08-01 14:36:46 -0300

    Add private api methods package (@mambasdk/bridges)

---
+++ packages/bridges/src/payment/index.js
@@ -0,0 +1,20 @@
+import mock from './mock.js';
+import addSharedTo from './shared.js';
+
+const PaymentApp = window.PaymentApp || {};
+
+/** For development environment */
+if (process.env.NODE_ENV !== 'production') {
+  addSharedTo(PaymentApp);
+  mock(PaymentApp);
+}
+
+/** For production environment */
+if (process.env.NODE_ENV === 'production') {
+  addSharedTo(PaymentApp);
+}
+
+/** Nullify the original exposed reference */
+window.App = null;
+
+export default PaymentApp;
