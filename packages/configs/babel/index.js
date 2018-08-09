4b7e9f75e0245c6fdd674802bed7527de7b3ec7f
Christian Kaisermann <christian@kaisermann.me>
2018-08-09 16:19:34 -0300

    Add debug env flag to preset-env debug property

--- packages/configs/babel/index.js
+++ packages/configs/babel/index.js
@@ -6,8 +6,8 @@ module.exports = {
         useBuiltIns: false,
         loose: true,
         /** Only parse modules if testing. If not, let bundler handle it */
-        modules: process.env.NODE_ENV === 'test' ? 'commonjs' : false,
-        debug: false,
+        modules: false,
+        debug: !!process.env.DEBUG,
         forceAllTransforms: true,
       },
     ],
@@ -16,6 +16,12 @@ module.exports = {
     /** Add support to import() */
     '@babel/plugin-syntax-dynamic-import',
     /** Add class properties support */
-    ['@babel/plugin-proposal-class-properties', { loose: false }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
   ],
+  env: {
+    test: {
+      /** Only for testing (NODE_ENV = 'test') in NodeJS, which needs CJS to work */
+      plugins: ['@babel/plugin-transform-modules-commonjs'],
+    },
+  },
 };
