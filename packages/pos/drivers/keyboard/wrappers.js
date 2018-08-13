d43761c3f23464b192dcb9bb66758031660604ff
Christian Kaisermann <christian@kaisermann.me>
2018-08-13 13:02:27 -0300

    Simplify simulator keyboard driver

--- packages/pos/drivers/keyboard/wrappers.js
+++ packages/pos/drivers/keyboard/wrappers.js
@@ -18,84 +18,66 @@ const KEYMAP = Object.freeze({
   56: '8',
   57: '9',
 });
-/**
- * Get the key code relative to a specific key name
- * @memberof Keyboard
- * @param {string} keyName - Key name
- * @returns {number} - Relative key code
- */
-function getKeyCode(keyName) {
-  return Object.keys(KEYMAP).find(code => KEYMAP[code] === keyName);
-}
-
-/**
- * Get the key name relative to a specific key code
- * @memberof Keyboard
- * @param {number} keyCode - Key code
- * @returns {string} - Relative key name
- */
-function getKeyName(keyCode) {
-  return KEYMAP[keyCode];
-}
-
-/**
- * Check if a certain key is a numeric key
- * @memberof Keyboard
- * @param {number} keyCode - Key code
- * @returns {boolean}
- */
-function isNumericKey(keyCode) {
-  return !Number.isNaN(parseFloat(KEYMAP[keyCode]));
-}
-
-/**
- * Check if a certain key is an action key
- * @memberof Keyboard
- * @param {number} keyCode - Key code
- * @returns {boolean}
- */
-function isActionKey(keyCode) {
-  return !isNumericKey(keyCode);
-}
-
-/**
- * Define if backspace button should be enabled
- */
-let _isBackspaceEnabled = true;
-
-/**
- * Return if the backspace button is enabled
- * @memberof Keyboard
- * @returns {boolean}
- */
-function isBackspaceEnabled() {
-  return _isBackspaceEnabled;
-}
-
-/**
- * Switch OFF the `isBackspaceEnabled` flag used by the front-end
- * @memberof Keyboard
- */
-function disableBackspace() {
-  _isBackspaceEnabled = false;
-}
-
-/**
- * Switch ON the `isBackspaceEnabled` flag used by the front-end
- * @memberof Keyboard
- */
-function enableBackspace() {
-  _isBackspaceEnabled = true;
-}
 
 export default function(driver) {
-  Object.assign(driver, {
-    getKeyCode,
-    getKeyName,
-    isNumericKey,
-    isActionKey,
-    isBackspaceEnabled,
-    disableBackspace,
-    enableBackspace,
-  });
+  /**
+   * Get the key code relative to a specific key name
+   * @memberof Keyboard
+   * @param {string} keyName - Key name
+   * @returns {number} - Relative key code
+   */
+  driver.getKeyCode = keyName =>
+    Object.keys(KEYMAP).find(code => KEYMAP[code] === keyName);
+
+  /**
+   * Get the key name relative to a specific key code
+   * @memberof Keyboard
+   * @param {number} keyCode - Key code
+   * @returns {string} - Relative key name
+   */
+  driver.getKeyName = keyCode => KEYMAP[keyCode];
+
+  /**
+   * Check if a certain key is a numeric key
+   * @memberof Keyboard
+   * @param {number} keyCode - Key code
+   * @returns {boolean}
+   */
+  driver.isNumericKey = keyCode => !Number.isNaN(parseFloat(KEYMAP[keyCode]));
+
+  /**
+   * Check if a certain key is an action key
+   * @memberof Keyboard
+   * @param {number} keyCode - Key code
+   * @returns {boolean}
+   */
+  driver.isActionKey = keyCode => !driver.isNumericKey(keyCode);
+
+  /**
+   * Define if backspace button should be enabled
+   */
+  let _isBackspaceEnabled = true;
+
+  /**
+   * Return if the backspace button is enabled
+   * @memberof Keyboard
+   * @returns {boolean}
+   */
+  driver.isBackspaceEnabled = () => _isBackspaceEnabled;
+
+  /**
+   * Switch OFF the `isBackspaceEnabled` flag used by the front-end
+   * @memberof Keyboard
+   */
+  driver.disableBackspace = () => {
+    _isBackspaceEnabled = false;
+  };
+
+  /**
+   * Switch ON the `isBackspaceEnabled` flag used by the front-end
+   * @memberof Keyboard
+   */
+  driver.enableBackspace = () => {
+    _isBackspaceEnabled = true;
+  };
 }
