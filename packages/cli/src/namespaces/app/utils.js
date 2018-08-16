c9a02d0e39ea192ccdb390de6e62ec5f8db8ffdf
Christian Kaisermann <christian@kaisermann.me>
2018-08-16 11:29:06 -0300

    Remove old code

--- packages/cli/src/namespaces/app/utils.js
+++ packages/cli/src/namespaces/app/utils.js
@@ -1,30 +1,5 @@
 const chalk = require('chalk');
 const { fromCwd } = require('quickenv');
-const { PKG, IS_WINDOWS } = require('../../consts.js');
-
-exports.validateProject = () => {
-  if (PKG == null) {
-    console.error(
-      chalk.red(`No "package.json" found. Is this an mamba app directory?"`),
-    );
-    process.exit(1);
-  }
-
-  if (typeof PKG.mamba === 'undefined') {
-    console.error(
-      chalk.red(
-        `No "mamba" property found at "${
-          PKG.filePath
-        }" Is this an mamba app directory?"`,
-      ),
-    );
-    process.exit(1);
-  }
-
-  /** Add the node_modules/.bin of the current app to the PATH env variable */
-  process.env.PATH +=
-    (IS_WINDOWS ? ';' : `:`) + fromCwd('node_modules', '.bin');
-};
 
 exports.validateTarget = target => {
   if (target !== 'pos' && target !== 'browser') {
