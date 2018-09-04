ae524c22542a00bb4892aaef552861dc4b0a6e17
Christian Kaisermann <christian@kaisermann.me>
2018-09-04 18:04:34 -0300

    Update dependencies and add css mq packer

--- packages/configs/postcss/index.js
+++ packages/configs/postcss/index.js
@@ -14,6 +14,7 @@ module.exports = {
     'postcss-atroot': {},
     'postcss-property-lookup': {},
     'postcss-nested': {},
+    'css-mqpacker': {},
     'postcss-reporter': { clearReportedMessages: true },
   },
 };
