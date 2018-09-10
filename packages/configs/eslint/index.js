174985a9802c4b57888c3d002d03d3e665c7dcbc
Christian Kaisermann <christian@kaisermann.me>
2018-09-10 11:04:01 -0300

    Dedupe no-params-reassign

--- packages/configs/eslint/index.js
+++ packages/configs/eslint/index.js
@@ -5,7 +5,7 @@ const IS_PROD = process.env.NODE_ENV === 'production';
 module.exports = {
   root: true,
   extends: ['airbnb-base', 'prettier', 'plugin:import/recommended'],
-  plugins: ['html', 'import', 'prettier'],
+  plugins: ['prettier', 'html', 'import'],
   settings: {
     'html/html-extensions': ['.html', '.svelte'],
   },
