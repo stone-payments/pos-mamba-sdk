69aeb4a1f43ee35a19dac1fc1ba8ed412b486538
Christian Kaisermann <christian@kaisermann.me>
2018-09-12 16:07:01 -0300

    Add format/lint/serve to mamba cli

--- packages/cli/src/namespaces/app/commands/serve.js
+++ packages/cli/src/namespaces/app/commands/serve.js
@@ -1,4 +1,4 @@
-const childProcess = require('child_process');
+const { runCmd } = require('../utils.js');
 
 /** Build the app for a specific environment */
 module.exports = {
@@ -6,6 +6,6 @@ module.exports = {
   desc: 'Start a local http server to deploy the app through the Develop app',
   handler() {
     const cmd = 'http-server "dist/" --cors="*"';
-    childProcess.execSync(cmd, { stdio: 'inherit' });
+    runCmd(cmd);
   },
 };
