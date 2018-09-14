6f61190f2e5b507bbf8ec30900bf5f8328f2264e
Christian Kaisermann <christian@kaisermann.me>
2018-09-14 17:51:13 -0300

    Add closed and opened signals

--- packages/pos/drivers/app/simulation.js
+++ packages/pos/drivers/app/simulation.js
@@ -2,9 +2,12 @@ import { log } from '../../simulator/libs/utils.js';
 
 export const NAMESPACE = 'App';
 
+export const SIGNALS = ['opened', 'closed'];
+
 export function setup(App) {
   App.doClose = () => {
     if (__DEV__) log('App closed');
+    App.closed();
   };
 
   App.getAppKey = () => '123456';
