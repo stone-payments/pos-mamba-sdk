72899af96606a92eb16e344f97ae7cba7e8c310f
Christian Kaisermann <christian@kaisermann.me>
2018-09-18 10:11:33 -0300

    Fix Merchant state path

--- packages/pos/simulator/libs/boot.js
+++ packages/pos/simulator/libs/boot.js
@@ -5,7 +5,7 @@ import * as Printer from '../../drivers/printer/simulation.js';
 import * as App from '../../drivers/app/simulation.js';
 import * as Storage from '../../drivers/storage/simulation.js';
 import * as Keyboard from '../../drivers/keyboard/simulation.js';
-import * as MbMerchant from '../../drivers/merchant/simulation.js';
+import * as Merchant from '../../drivers/merchant/simulation.js';
 import * as Payment from '../../drivers/payment/simulation.js';
 import * as System from '../../drivers/system/simulation.js';
 
@@ -21,7 +21,7 @@ attachDrivers({
   App,
   Storage,
   Keyboard,
-  MbMerchant,
+  Merchant,
   Payment,
   System,
   Http,
