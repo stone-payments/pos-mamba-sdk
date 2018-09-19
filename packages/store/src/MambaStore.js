706f6f66e351d5674a6f21390c4a840405e041eb
Christian Kaisermann <christian@kaisermann.me>
2018-09-19 15:08:34 -0300

    Move store.meta to <App/> component

--- packages/store/src/MambaStore.js
+++ packages/store/src/MambaStore.js
@@ -1,13 +1,12 @@
 import { Store } from 'svelte/store.js';
 import { setDeep, getDeep } from 'svelte-extras';
-import { INITIAL_META_DATA } from './meta.js';
 
 const LOCAL_STORAGE_KEY = 'MambaStore';
 
 export default class MambaStore extends Store {
   constructor(data = {}) {
     /** Get persisted data from localStorage if available */
-    if (localStorage) {
+    if (__DEV__ && localStorage) {
       const persistedStore = localStorage.getItem(LOCAL_STORAGE_KEY);
       if (persistedStore != null) {
         /** Get persisted data */
@@ -15,14 +14,11 @@ export default class MambaStore extends Store {
       }
     }
 
-    /** Initialize the app meta data */
-    Object.assign(data, INITIAL_META_DATA);
-
     /** Initialize the actual store */
     super(data);
 
     /** Persist store changes to localStorage */
-    if (localStorage) {
+    if (__DEV__ && localStorage) {
       this.on('state', ({ current }) => {
         localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(current));
       });
