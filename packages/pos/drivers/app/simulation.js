4a1b68171e2bf4aedd8ec4cbde2b83c243a97d5e
Christian Kaisermann <christian@kaisermann.me>
2018-09-20 14:37:34 -0300

    Remove old App simulation. Remove unused helper

--- packages/pos/drivers/app/simulation.js
+++ packages/pos/drivers/app/simulation.js
@@ -13,37 +13,4 @@ export function setup(App) {
   App.getAppKey = () => '123456';
 
   App.isRunningOnDevice = () => false;
-
-  App.listApps = () => [
-    {
-      id: 1,
-      name: 'app1',
-      defaultName: 'app1',
-      icon: '',
-    },
-    {
-      id: 2,
-      name: 'app2',
-      defaultName: 'app2',
-      icon: '',
-    },
-  ];
-
-  App.downloadAndSave = (tar, destination) => {
-    if (__DEV__) {
-      log(`downloading tar: ${tar}`);
-      log(`saving in: ${destination}`);
-    }
-  };
-
-  App.installApp = (tarPath, destination) => {
-    if (__DEV__) {
-      log(`tarPath: ${tarPath}`);
-      log(`destination: ${destination}`);
-    }
-  };
-
-  App.deleteApp = appId => {
-    if (__DEV__) log(`appId: ${appId}`);
-  };
 }
