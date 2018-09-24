1798ccdc5d40378dfb7c380e0376166b1a3e762a
Christian Kaisermann <christian@kaisermann.me>
2018-09-24 11:24:22 -0300

    Fix isPrinting simulation

--- packages/pos/drivers/printer/simulation.js
+++ packages/pos/drivers/printer/simulation.js
@@ -3,7 +3,9 @@ import { State } from '../../simulator/libs/main.js';
 export const NAMESPACE = '$Printer';
 
 export const SETTINGS = {
-  shouldFail: false,
+  panel: {
+    shouldFail: false,
+  },
   isPrinting: false,
   paperWidth: 384,
 };
@@ -13,15 +15,17 @@ export const SIGNALS = ['printerDone'];
 export function setup(Printer) {
   Printer.getPaperWidth = () => State.get('$Printer.paperWidth');
   Printer.isPrinting = () => State.get('$Printer.isPrinting');
-  Printer.failedPrinting = () => State.get('$Printer.shouldFail');
-  Printer.test = () => State.set('$Printer.shouldFail', true);
+  Printer.failedPrinting = () => State.get('$Printer.panel.shouldFail');
 
   Printer.doPrint = function doPrint(content, options) {
+    State.set('$Printer.isPrinting', true);
+
     setTimeout(() => {
       /** Fire the printing signal for the browser mamba simulation */
       if (!Printer.failedPrinting()) {
         State.print(content, options);
       }
+      State.set('$Printer.isPrinting', false);
       Printer.printerDone();
     }, 1500);
   };
