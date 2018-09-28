6dc29b1afef192af42748006451d5ae1529ad78b
Christian Kaisermann <christian@kaisermann.me>
2018-09-28 11:21:33 -0300

    Refactor base driver to work with signal listener groups

--- packages/pos/drivers/base.js
+++ packages/pos/drivers/base.js
@@ -1,98 +1,156 @@
-export default {
-  _listening: {},
+export default () => {
+  const defaultGroup = {};
+  const groups = { default: defaultGroup };
+  let currentGroupName = 'default';
+  let currentGroup = defaultGroup;
 
-  _isSlotFilled(signal, callback) {
-    const callbackList = this._listening[signal];
+  const isSlotFilled = (signal, callback) => {
+    const callbackList = currentGroup[signal];
     return (
       callbackList && callbackList.length && callbackList.indexOf(callback) > -1
     );
-  },
+  };
 
-  /** Connect a callback to a slot */
-  on(signal, callback) {
-    if (typeof callback === 'function') {
-      if (!this._listening[signal]) {
-        this._listening[signal] = [];
+  return {
+    getGroups: () => groups,
+    group(groupName) {
+      currentGroupName = groupName;
+      if (groups[groupName]) {
+        currentGroup = groups[groupName];
+      } else {
+        currentGroup = {};
+        groups[groupName] = currentGroup;
       }
-      this._listening[signal].push(callback);
-      this[signal].connect(callback);
-    }
-    return this;
-  },
+      return this;
+    },
 
-  /** Disconnect a callback from a slot */
-  off(signal, callback) {
-    /** If no callback passed, disconnect all slots from the signal */
-    if (typeof callback === 'undefined') {
-      this._listening[signal].forEach(slotCallback => {
-        this[signal].disconnect(slotCallback);
-      });
-      this._listening[signal] = [];
-    } else {
-      const slotIndex = this._listening[signal].indexOf(callback);
+    endGroup() {
+      currentGroup = defaultGroup;
+      return this;
+    },
 
-      if (slotIndex > -1) {
-        this[signal].disconnect(this._listening[signal][slotIndex]);
-        this._listening[signal].splice(slotIndex, 1);
-      } else if (__DEV__) {
-        console.warn(
-          '[@mamba/pos/driver] Tried to disconnect a non-connected slot.',
+    /** Connect a callback to a slot */
+    on(signal, callback) {
+      if (typeof callback === 'function') {
+        if (!currentGroup[signal]) {
+          currentGroup[signal] = [];
+        }
+        currentGroup[signal].push(callback);
+
+        this[signal].connect(callback);
+      }
+      return this;
+    },
+
+    /** Disconnect a callback from a slot */
+    off(signal, callback) {
+      const group = currentGroup;
+      /** If no callback passed, disconnect all slots from the signal */
+      if (typeof callback === 'undefined') {
+        for (let i = group[signal].length; i--; ) {
+          this[signal].disconnect(group[signal][i]);
+        }
+        delete group[signal];
+      } else {
+        const slotIndex = group[signal].findIndex(
+          slotCallback => slotCallback === callback,
         );
-      }
-    }
-    return this;
-  },
 
-  /** Allow only a unique callback on a specific slot */
-  unique(signal, callback) {
-    if (this._isSlotFilled(signal, callback)) {
-      this.off(signal, callback);
-    }
-    return this.on(signal, callback);
-  },
+        if (slotIndex > -1) {
+          this[signal].disconnect(group[signal][slotIndex]);
 
-  /** Execute once a callback when the signal is dispatched and disconnect from it */
-  once(signal, callback) {
-    return this.race([[signal, callback]]);
-  },
+          group[signal].splice(slotIndex, 1);
 
-  /** The first signal dispatched is executed and automatically cancel all others */
-  race(entries) {
-    const wrappedCallbacks = {};
-    entries.forEach(([signal, callback]) => {
-      if (__DEV__) {
-        console.log(`Connecting once '${signal}'`);
+          /** Delete the signal slot list if it's empty */
+          if (group[signal].length === 0) {
+            delete group[signal];
+          }
+        } else if (__DEV__) {
+          console.warn(
+            `[@mamba/pos/driver] Tried to disconnect from "${signal}" a non-connected slot (group: ${currentGroupName}).`,
+          );
+        }
       }
 
-      /** If the signal's slot is already filled with a callback, disconnect it */
-      if (this._isSlotFilled(signal, callback)) {
+      /** If the current signal group is empty, let's delete it */
+      if (currentGroup !== defaultGroup && Object.keys(group).length === 0) {
+        delete groups[currentGroupName];
+        this.endGroup();
+      }
+
+      return this;
+    },
+
+    /** Allow only a unique callback on a specific slot */
+    unique(signal, callback) {
+      if (isSlotFilled(signal, callback)) {
         this.off(signal, callback);
       }
+      return this.on(signal, callback);
+    },
 
-      /** Wrap the signal callback to disconnect all slots once one of the signals are emitted */
-      wrappedCallbacks[signal] = () => {
-        callback();
-        Object.keys(wrappedCallbacks).forEach(signalName => {
-          if (__DEV__) {
-            console.log(`Removing '${signalName}'`);
-          }
-          this.off(signalName, wrappedCallbacks[signalName]);
-        });
-      };
+    /** Execute once a callback when the signal is dispatched and disconnect from it */
+    once(signal, callback) {
+      return this.race([[signal, callback]]);
+    },
 
-      /** Listen to the signal emission */
-      this.on(signal, wrappedCallbacks[signal]);
-    });
+    /** The first signal dispatched is executed and automatically cancel all others */
+    race(entries) {
+      const wrappedCallbacks = {};
+      entries.forEach(([signal, callback]) => {
+        if (__DEV__) {
+          console.log(`Connecting once '${signal}'`);
+        }
 
-    return this;
-  },
+        /** If the signal's slot is already filled with a callback, disconnect it */
+        if (isSlotFilled(signal, callback)) {
+          this.off(signal, callback);
+        }
 
-  destroy() {
-    Object.keys(this._listening).forEach(signal => {
+        /** Wrap the signal callback to disconnect all slots once one of the signals are emitted */
+        wrappedCallbacks[signal] = () => {
+          callback();
+          Object.keys(wrappedCallbacks).forEach(signalName => {
+            if (__DEV__) {
+              console.log(`Removing '${signalName}'`);
+            }
+            this.off(signalName, wrappedCallbacks[signalName]);
+          });
+        };
+
+        /** Listen to the signal emission */
+        this.on(signal, wrappedCallbacks[signal]);
+      });
+
+      return this;
+    },
+
+    /** Destroy all signal listeners from a specified group */
+    destroyGroup(groupName = 'default') {
       if (__DEV__) {
-        console.log(`Disconnecting all slots from '${signal}'`);
+        console.log(`Destroying signal group: ${groupName}`);
       }
-      this.off(signal);
-    });
-  },
+
+      if (!groups[groupName]) {
+        console.error(
+          `[@mamba/pos/driver] Destroying non existing group: '${groupName}'`,
+        );
+      }
+      this.group(groupName);
+
+      Object.keys(groups[groupName]).forEach(signal => {
+        if (__DEV__) {
+          console.log(`Disconnecting all slots from signal '${signal}'.`);
+        }
+        this.off(signal);
+      });
+
+      this.endGroup();
+    },
+
+    /** Destroy all signal listeners from all groups */
+    destroy() {
+      Object.keys(groups).forEach(group => this.destroyGroup(group));
+    },
+  };
 };
