1fee20518f058dda954447dc6d32d3a0431cc5ae
Christian Kaisermann <christian@kaisermann.me>
2018-10-04 22:19:34 -0300

    Change two .once to a .race

--- packages/pos/drivers/http/wrappers.js
+++ packages/pos/drivers/http/wrappers.js
@@ -1,12 +1,10 @@
 export default function(driver) {
   driver.send = function send(configParams) {
     return new Promise((resolve, reject) => {
-      driver.once('requestFailed', () => {
-        return reject(driver.getError());
-      });
-      driver.once('requestFinished', () => {
-        resolve(driver.getData());
-      });
+      driver.race([
+        ['requestFailed', () => reject(driver.getError())],
+        ['requestFinished', () => resolve(driver.getData())],
+      ]);
 
       /** Asynchronously make a request at the backend */
       driver.doSend(configParams);
