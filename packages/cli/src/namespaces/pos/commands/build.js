1ce367b721793c68a75b486e547c13acfd837270
pedroglp <pedrogabriellancelloti@poli.ufrj.br>
2018-10-22 16:32:22 -0300

    Add lib flag option and clean. Fixes always clean build.

--- packages/cli/src/namespaces/pos/commands/build.js
+++ packages/cli/src/namespaces/pos/commands/build.js
@@ -1,4 +1,6 @@
-const { runCmd } = require('../../../utils.js');
+const {
+  runCmd,
+} = require('../../../utils.js');
 
 const config = require('../buildConfig.json');
 
@@ -17,8 +19,23 @@ module.exports = {
       default: false,
       alias: ['r'],
     },
+    lib: {
+      description: 'Build only the .pro file at the path passed.',
+      type: 'string',
+      default: 'MAMBA.pro',
+      alias: ['l', '--lib'],
+    },
+    clean: {
+      description: 'Clears before building.',
+      default: false,
+      alias: ['--clean'],
+    },
   },
-  handler({ release }) {
+  handler({
+    release,
+    lib,
+    clean,
+  }) {
     console.log('Building Mamba System');
 
     /** Set compiler PATH */
@@ -29,11 +46,16 @@ module.exports = {
     /** Build project */
     runCmd([
       'cd $MAMBA',
-      `${qtPath}/bin/qmake MAMBA.pro -r -spec ${qtMkConf} ${
+      `${qtPath}/bin/qmake ${lib} -r -spec ${qtMkConf} ${
         release ? '' : 'CONFIG+=debug'
       }`,
     ]);
-    runCmd(['cd $MAMBA', 'make clean', 'make -j$(nproc)']);
+
+    if (clean) {
+      runCmd(['cd $MAMBA', 'make clean', 'make -j$(nproc)']);
+    } else {
+      runCmd(['cd $MAMBA', 'make -j$(nproc)']);
+    }
 
     /** Generate DB Files */
     console.log('Building Mamba Database');
@@ -49,8 +71,7 @@ module.exports = {
         `cp ${qtPath}/lib/*.so* ${destDir}/lib`,
         `cp ${qtPath}/plugins ${destDir}`,
         `cp ${qtPath}/imports ${destDir}`,
-      ],
-      {
+      ], {
         exit: false,
       },
     );
