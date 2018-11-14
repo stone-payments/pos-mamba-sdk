1aeb3c1e16be93d5bf19d9d469c49dbafe8033b5
Christian Kaisermann <christian.kaisermann@stone.com.br>
2018-11-14 19:06:39 -0200

    Add .toggle() method to <Switch/>

--- packages/components/Switch/Switch.test.js
+++ packages/components/Switch/Switch.test.js
@@ -13,22 +13,36 @@ const newInstance = data => {
 
 const getCheckboxNode = () => target.querySelector('input[type=checkbox]');
 
-describe('behavior', () => {
-  it('should be able to start checked', () => {
-    newInstance({ checked: true });
-    expect(getCheckboxNode().checked).toBe(true);
-  });
+it('should be able to start checked', () => {
+  newInstance({ checked: true });
+  expect(getCheckboxNode().checked).toBe(true);
+});
 
-  it('should be able to disable the checkbox', () => {
-    newInstance({ disabled: true });
-    expect(getCheckboxNode().disabled).toBe(true);
-  });
+it('should be able to disable the checkbox', () => {
+  newInstance({ disabled: true });
+  expect(getCheckboxNode().disabled).toBe(true);
+});
 
-  it('should fire a change event when value is modified', () => {
-    newInstance();
-    return new Promise(res => {
-      component.on('change', res);
-      getCheckboxNode().click();
-    });
+it('should fire a change event when value is modified', () => {
+  newInstance();
+  return new Promise(res => {
+    component.on('change', res);
+    getCheckboxNode().click();
   });
+});
+
+it('should toggle its checked value', () => {
+  newInstance();
+
+  component.toggle();
+  expect(getCheckboxNode().checked).toBe(true);
+
+  component.toggle(true);
+  expect(getCheckboxNode().checked).toBe(true);
+
+  component.toggle(false);
+  expect(getCheckboxNode().checked).toBe(false);
+
+  component.toggle(null);
+  expect(getCheckboxNode().checked).toBe(false);
 });
