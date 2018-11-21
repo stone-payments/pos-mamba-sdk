c0b5d045438cfd6f03a1fb729b582758cb768bd7
Christian Kaisermann <christian.kaisermann@stone.com.br>
2018-11-21 14:43:26 -0200

    Convert sprite test to new env

--- packages/components/Sprite/Sprite.test.js
+++ packages/components/Sprite/Sprite.test.js
@@ -1,17 +1,8 @@
 import Sprite from './Sprite.html';
 
-const target = document.body;
+const { newComponent } = global;
+
 let component;
-
-const newInstance = data => {
-  if (component) {
-    component.destroy();
-  }
-  component = new Sprite({ target, data });
-  return component;
-};
-
-newInstance({ src: './example/static/stone.png' });
 
 beforeAll(() => {
   Object.defineProperty(Image.prototype, 'src', {
@@ -21,30 +12,24 @@ beforeAll(() => {
   });
 });
 
-describe('behavior', () => {
-  beforeAll(() => {
-    newInstance({ src: './example/static/stone.png' });
-  });
-
-  it('should automatically start after image loading', () => {
-    expect(typeof component.get()._interval).toBe('number');
+beforeEach(() => {
+  component = newComponent(Sprite, {
+    data: { src: './example/static/stone.png' },
   });
 });
 
-describe('methods', () => {
-  beforeAll(() => {
-    newInstance({ src: './example/static/stone.png' });
-  });
+it('should automatically start after image loading', () => {
+  expect(typeof component.get()._interval).toBe('number');
+});
 
-  it('should stop the sprite animation interval', () => {
-    expect(typeof component.stop).toBe('function');
-    component.stop();
-    expect(component.get()._interval).toBe(null);
-  });
+it('should stop the sprite animation interval', () => {
+  expect(typeof component.stop).toBe('function');
+  component.stop();
+  expect(component.get()._interval).toBe(null);
+});
 
-  it('should start the sprite animation interval', () => {
-    expect(typeof component.start).toBe('function');
-    component.start();
-    expect(typeof component.get()._interval).toBe('number');
-  });
+it('should start the sprite animation interval', () => {
+  expect(typeof component.start).toBe('function');
+  component.start();
+  expect(typeof component.get()._interval).toBe('number');
 });
