61ccd7f0cc528d2f07c8a0fa59364d5bd7899926
Christian Kaisermann <christian.kaisermann@stone.com.br>
2018-11-27 11:14:20 -0200

    Fix typo and small refactor

--- tools/jest/babelPreprocess.js
+++ tools/jest/babelPreprocess.js
@@ -1,6 +1,7 @@
 /**
  * Create a custom transformer to use a shared '.babelrc.js' between all workspaces
  * */
-module.exports = require('babel-jest').createTransformer(
-  require('../../.babelrc.js'),
-);
+
+const babelConfig = require('../../.babelrc.js');
+
+module.exports = require('babel-jest').createTransformer(babelConfig);
