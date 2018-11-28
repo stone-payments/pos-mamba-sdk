4a1ecb61f61a29bc0dc5f264b7524b7035494d7f
pedroglp <pedrogabriellancelloti@poli.ufrj.br>
2018-11-28 18:02:32 -0200

    Add MoneyInput test

---
+++ packages/components/Input/Money.test.js
@@ -0,0 +1,85 @@
+import MoneyInput from './Money.html';
+import formatMoney from './libs/formatMoney.js';
+import System from '../../pos/api/system.js';
+
+const { newTestRoot, fireKey } = global;
+
+const root = newTestRoot();
+let moneyInput;
+
+const newMoneyInput = data => root.createComponent(MoneyInput, { data });
+
+beforeAll(() => {
+  moneyInput = newMoneyInput({});
+});
+
+describe('Component Standards', () => {
+  it('should have maximum value of 99999999.99', () => {
+    const { limit } = moneyInput.get();
+    expect(limit).toBe(99999999.99);
+  });
+
+  it('should have by default prefix in BRL', () => {
+    const { prefix } = moneyInput.get();
+    expect(prefix).toBe('R$ ');
+  });
+
+  it('should have by default suffix empty suffix', () => {
+    const { suffix } = moneyInput.get();
+    expect(suffix).toBe('');
+  });
+
+  it('should have raw value of 0 by default', () => {
+    const { rawValue } = moneyInput.get();
+    expect(rawValue).toBe('0');
+  });
+});
+
+describe('Behavior', () => {
+  it('should add prefix in displayed value', () => {
+    expect(root.query('input').value.split(0, 2)).toContain('R$ ');
+  });
+
+  it('should add suffix to value', () => {
+    moneyInput.set({ suffix: 'a' });
+    expect(root.query('input').value.endsWith('a')).toBe(true);
+  });
+
+  describe('Format Money', () => {
+    it('should return a string', () => {
+      expect(typeof formatMoney(100)).toBe('string');
+    });
+    it('should separate cents using comma', () => {
+      expect(formatMoney(100).split(',').length).toBe(2);
+    });
+    it('should add . every 3 digits', () => {
+      expect(formatMoney(100)).not.toContain('.');
+      expect(formatMoney(10000)).toContain('.');
+    });
+  });
+
+  describe('Keypressed Actions', () => {
+    it('should block inputs if input is readonly', () => {});
+    it('should remove last character when backspace is pressed', () => {});
+    it('should set rawValue to 0 if last character is removed', () => {});
+    it('should not accept non numeric inputs', () => {});
+    it('should not accept values bigger than limit', () => {});
+    it('should update raw value on first input', () => {});
+    it('should update raw value on new input', () => {});
+  });
+
+  describe('Submit', () => {
+    it('should fire submit if value is bigger than 0', () =>
+      new Promise(res => {
+        moneyInput.on('submit', res);
+        moneyInput.set({ rawValue: '1' });
+        fireKey('enter', moneyInput.refs.amountInput.refs.input);
+      }));
+    it('should beep if value is lower or equal to 0', () =>
+      new Promise(res => {
+        System.beep = res;
+        moneyInput.set({ rawValue: '0' });
+        fireKey('enter', moneyInput.refs.amountInput.refs.input);
+      }));
+  });
+});
