bef3a5ae4b04bcd9c4d21cc6b86aaec41525d9f9
Christian Kaisermann <christian.kaisermann@stone.com.br>
2018-11-30 12:53:45 -0200

    Move MoneyInput caret to end of text at every focus

--- packages/components/Input/Money.test.js
+++ packages/components/Input/Money.test.js
@@ -5,15 +5,21 @@ import System from '../../pos/api/system.js';
 const { newTestRoot, fireKey, typeOn } = global;
 
 const root = newTestRoot();
+
+const newMoneyInput = data =>
+  root.createComponent(MoneyInput, { unique: true, data });
+
 let moneyInput;
 
-const newMoneyInput = data => root.createComponent(MoneyInput, { data });
+const getInputEl = () => moneyInput.refs.amountInput.refs.input;
 
-beforeAll(() => {
-  moneyInput = newMoneyInput({});
-});
+const type = what => typeOn(getInputEl(), what);
 
 describe('Component Standards', () => {
+  beforeAll(() => {
+    moneyInput = newMoneyInput();
+  });
+
   it('should have maximum value of 99999999.99', () => {
     const { limit } = moneyInput.get();
     expect(limit).toBe(99999999.99);
@@ -37,21 +43,23 @@ describe('Component Standards', () => {
 
 describe('Behavior', () => {
   it('should add prefix in displayed value', () => {
-    expect(root.query('input').value.split(0, 2)).toContain('R$ ');
+    expect(getInputEl().value.split(0, 2)).toContain('R$ ');
   });
 
   it('should add suffix to value', () => {
     moneyInput.set({ suffix: 'a' });
-    expect(root.query('input').value.endsWith('a')).toBe(true);
+    expect(getInputEl().value.endsWith('a')).toBe(true);
   });
 
   describe('Format Money', () => {
     it('should return a string', () => {
       expect(typeof formatMoney(100)).toBe('string');
     });
+
     it('should separate cents using comma', () => {
       expect(formatMoney(100).split(',').length).toBe(2);
     });
+
     it('should add . every 3 digits', () => {
       expect(formatMoney(100)).not.toContain('.');
       expect(formatMoney(10000)).toContain('.');
@@ -59,36 +67,60 @@ describe('Behavior', () => {
   });
 
   describe('Keypressed Actions', () => {
+    beforeAll(() => {
+      moneyInput = newMoneyInput();
+    });
+
     it('should update raw value on first input', () => {
-      moneyInput.set({ rawValue: '0' });
-      fireKey(moneyInput.refs.amountInput.refs.input, '1');
+      type('1');
       expect(moneyInput.get().rawValue).toBe('1');
     });
+
     it('should update raw value on new input', () => {
-      fireKey(moneyInput.refs.amountInput.refs.input, '1');
+      type('1');
       expect(moneyInput.get().rawValue).toBe('11');
     });
+
     it('should remove last character when backspace is pressed', () => {
-      fireKey(moneyInput.refs.amountInput.refs.input, 'back');
+      fireKey(getInputEl(), 'back');
       expect(moneyInput.get().rawValue).toBe('1');
     });
+
     it('should set rawValue to 0 if last character is removed', () => {
-      fireKey(moneyInput.refs.amountInput.refs.input, 'back');
+      fireKey(getInputEl(), 'back');
       expect(moneyInput.get().rawValue).toBe('0');
     });
+
     it('should not accept non numeric inputs', () => {
-      fireKey(moneyInput.refs.amountInput.refs.input, 'a');
+      type('a');
       expect(moneyInput.get().rawValue).toBe('0');
     });
+
     it('should block inputs if input is readonly', () => {
       moneyInput.set({ readonly: true });
-      fireKey(moneyInput.refs.amountInput.refs.input, '1');
+      type('1');
       expect(moneyInput.get().rawValue).toBe('0');
     });
+
     it('should not accept values bigger than limit', () => {
       moneyInput.set({ readonly: false });
-      typeOn(moneyInput.refs.amountInput.refs.input, '9999999999999');
+      type('9999999999999');
       expect(moneyInput.get().rawValue).toBe('9999999999');
+    });
+
+    it('should move the caret to the right side of the input on every focus', () => {
+      moneyInput = newMoneyInput();
+      type('11');
+      getInputEl().setSelectionRange(0, 0);
+      moneyInput.blur();
+
+      return new Promise(res => {
+        setTimeout(() => {
+          const { value } = moneyInput.get();
+          expect(getInputEl().selectionEnd).toBe(value.length);
+          res();
+        });
+      });
     });
   });
 
@@ -97,13 +129,13 @@ describe('Behavior', () => {
       new Promise(res => {
         moneyInput.on('submit', res);
         moneyInput.set({ rawValue: '1' });
-        fireKey(moneyInput.refs.amountInput.refs.input, 'enter');
+        fireKey(getInputEl(), 'enter');
       }));
     it('should beep if value is lower or equal to 0', () =>
       new Promise(res => {
         System.beep = res;
         moneyInput.set({ rawValue: '0' });
-        fireKey(moneyInput.refs.amountInput.refs.input, 'enter');
+        fireKey(getInputEl(), 'enter');
       }));
   });
 });
