262c44159b4ef7295581fa10ba6f34fb28ec7197
Christian Kaisermann <christian.kaisermann@stone.com.br>
2018-11-30 18:04:39 -0200

    Add meta tags for better mobile experience

--- packages/webpack/helpers/getHTMLTemplate.js
+++ packages/webpack/helpers/getHTMLTemplate.js
@@ -3,11 +3,15 @@ const { minify: htmlMinifier } = require('html-minifier');
 
 const { generateCSSReferences, generateJSReferences } = MiniHtmlWebpackPlugin;
 
+const { IS_BROWSER } = require('./consts.js');
+
 module.exports = ({ css, js, title, publicPath }) => {
   const htmlTemplate = `<!DOCTYPE html>
 <html>
   <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+    ${IS_BROWSER ? '<meta name="mobile-web-app-capable" content="yes">' : ''}
     <title>${title}</title>
     ${generateCSSReferences(css, publicPath)}
   </head>
