6e7e7c07d861e3606955ad5b27efa20ab5b28e8c
Jackson Garcia <jackson.garcia@stone.com.br>
2018-12-21 19:08:41 -0200

    add type equals

--- packages/pos/drivers/http/wrappers.js
+++ packages/pos/drivers/http/wrappers.js
@@ -1,20 +1,20 @@
 export default function(driver) {
   driver.send = function send(configParams) {
     return new Promise((resolve, reject) => {
-      const refSignal = Math.random() * new Date().getMilliseconds();
+      const refSignal = `${Math.random() * new Date().getMilliseconds()}`;
 
       driver.race([
         [
           'requestFailed',
           (err, id) => {
-            if (refSignal != id) return false;
+            if (refSignal !== id) return false;
             reject(err);
           },
         ],
         [
           'requestFinished',
           (data, id) => {
-            if (refSignal != id) return false;
+            if (refSignal !== id) return false;
             resolve(data);
           },
         ],
