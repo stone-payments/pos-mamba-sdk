a4704922f03ce5f210907ad1f9d9305a3882e6b9
Jackson Garcia <jackson.garcia@stone.com.br>
2018-12-21 18:37:06 -0200

    Async request and send id

--- packages/pos/drivers/http/wrappers.js
+++ packages/pos/drivers/http/wrappers.js
@@ -1,13 +1,27 @@
 export default function(driver) {
   driver.send = function send(configParams) {
     return new Promise((resolve, reject) => {
+      const refSignal = Math.random() * new Date().getMilliseconds();
+
       driver.race([
-        ['requestFailed', () => reject(driver.getError())],
-        ['requestFinished', () => resolve(driver.getData())],
+        [
+          'requestFailed',
+          (err, id) => {
+            if (refSignal != id) return false;
+            reject(err);
+          },
+        ],
+        [
+          'requestFinished',
+          (data, id) => {
+            if (refSignal != id) return false;
+            resolve(data);
+          },
+        ],
       ]);
 
       /** Asynchronously make a request at the backend */
-      driver.doSend(configParams);
+      driver.doSend(configParams, refSignal);
     });
   };
 }
