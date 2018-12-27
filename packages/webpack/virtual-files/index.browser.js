d63eb108033b6c505053031115850ffdec005f02
Christian Kaisermann <christian.kaisermann@stone.com.br>
2018-12-27 16:03:18 -0200

    De-async index.browser

--- packages/webpack/virtual-files/index.browser.js
+++ packages/webpack/virtual-files/index.browser.js
@@ -1,4 +1,4 @@
-import { System, AppManager } from '@mamba/pos/simulator/index.js';
+import { AppManager } from '@mamba/pos/simulator/index.js';
 import icon from '__APP_ICON__'; // eslint-disable-line
 import App from './index.html';
 
@@ -8,13 +8,5 @@ const manifest = {
   icon,
 };
 
-const init = () => {
-  AppManager.installApp(App, manifest);
-  AppManager.open(manifest.slug);
-};
-
-if (System.POS && typeof System.POS.then === 'function') {
-  System.on('viewLoad', init);
-} else {
-  init();
-}
+AppManager.installApp(App, manifest);
+AppManager.open(manifest.slug);
