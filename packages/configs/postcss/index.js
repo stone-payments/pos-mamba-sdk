d8ce629123f0b45d819ace6e19b4129455fb67f4
Christian Kaisermann <christian.kaisermann@stone.com.br>
2019-01-10 17:20:30 -0200

    Remove unused postcss plugins

--- packages/configs/postcss/index.js
+++ packages/configs/postcss/index.js
@@ -6,29 +6,26 @@ const postcssExtendRule = require('postcss-extend-rule');
 const postcssAdvancedVariables = require('postcss-advanced-variables');
 const postcssCalc = require('postcss-calc');
 const postcssPresetEnv = require('postcss-preset-env');
-const postcssAtroot = require('postcss-atroot');
-const postcssPropertyLookup = require('postcss-property-lookup');
 const postcssNested = require('postcss-nested');
-const cssMqpacker = require('css-mqpacker');
+const cssMqPacker = require('css-mqpacker');
 const postcssReporter = require('postcss-reporter');
 const postcssHexRGBA = require('postcss-hexrgba');
 const prependImports = require('./prependImports.js');
 
-const IS_BUILDING_APP = !!process.env.APP_ENV;
-
 const CWD = process.cwd();
 
+const IS_BUILDING_APP = !!process.env.APP_ENV;
 const LOCAL_THEME_PATH = resolve(CWD, 'src/theme.pcss');
 
 const THEME_FILES = [
   '@mamba/styles/theme.pcss',
-  existsSync(LOCAL_THEME_PATH) && LOCAL_THEME_PATH,
+  IS_BUILDING_APP && existsSync(LOCAL_THEME_PATH) && LOCAL_THEME_PATH,
 ];
 
 module.exports = {
   plugins: [
     /** Custom plugin to prepend theme imports ONLY for apps */
-    IS_BUILDING_APP && prependImports({ files: THEME_FILES }),
+    prependImports({ files: THEME_FILES }),
     postcssEasyImport({
       extensions: ['.css', '.pcss'],
     }),
@@ -46,10 +43,8 @@ module.exports = {
         },
       },
     }),
-    postcssAtroot(),
-    postcssPropertyLookup(),
     postcssNested(),
-    cssMqpacker(),
+    cssMqPacker(),
     postcssReporter({ clearReportedMessages: true }),
   ],
 };
