f2fa9a1a529a115af8ea907ac2a84806e2aeae64
Christian Kaisermann <christian.kaisermann@stone.com.br>
2019-02-04 19:01:49 -0200

    internal: 🏭 Add explicit simulated card events
      Add events: `cardInserted`, `cardRemoved`, `cardToggled`

--- changelog.config.js
+++ changelog.config.js
@@ -1,15 +1,64 @@
+const COMMIT_TYPES = {
+  chore: {
+    description: 'Build process or auxiliary tool changes',
+    emoji: '🤖',
+    value: 'chore',
+  },
+  ci: {
+    description: 'CI related changes',
+    emoji: '🎡',
+    value: 'ci',
+  },
+  docs: {
+    description: 'Documentation only changes',
+    emoji: '✏️',
+    value: 'docs',
+  },
+  feat: {
+    description: 'A new feature',
+    emoji: '🎸',
+    value: 'feat',
+  },
+  fix: {
+    description: 'A bug fix',
+    emoji: '🐛',
+    value: 'fix',
+  },
+  perf: {
+    description: 'A code change that improves performance',
+    emoji: '⚡️',
+    value: 'perf',
+  },
+  refactor: {
+    description: 'A code change that neither fixes a bug or adds a feature',
+    emoji: '💡',
+    value: 'refactor',
+  },
+  release: {
+    description: 'Create a release commit',
+    emoji: '🏹',
+    value: 'release',
+  },
+  style: {
+    description: 'Markup, white-space, formatting, missing semi-colons...',
+    emoji: '💄',
+    value: 'style',
+  },
+  test: {
+    description: 'Adding missing tests',
+    emoji: '💍',
+    value: 'test',
+  },
+  internal: {
+    description:
+      'Internal refactor/change/feature not to be used by third parties',
+    emoji: '🏭',
+    value: 'internal',
+  },
+};
+
 module.exports = {
-  list: [
-    'test',
-    'feat',
-    'fix',
-    'chore',
-    'docs',
-    'refactor',
-    'style',
-    'ci',
-    'perf',
-  ],
+  list: Object.keys(COMMIT_TYPES),
   maxMessageLength: 100,
   minMessageLength: 0,
   questions: [
@@ -22,56 +71,5 @@ module.exports = {
     'lerna',
   ],
   scopes: [],
-  types: {
-    chore: {
-      description: 'Build process or auxiliary tool changes',
-      emoji: '🤖',
-      value: 'chore',
-    },
-    ci: {
-      description: 'CI related changes',
-      emoji: '🎡',
-      value: 'ci',
-    },
-    docs: {
-      description: 'Documentation only changes',
-      emoji: '✏️',
-      value: 'docs',
-    },
-    feat: {
-      description: 'A new feature',
-      emoji: '🎸',
-      value: 'feat',
-    },
-    fix: {
-      description: 'A bug fix',
-      emoji: '🐛',
-      value: 'fix',
-    },
-    perf: {
-      description: 'A code change that improves performance',
-      emoji: '⚡️',
-      value: 'perf',
-    },
-    refactor: {
-      description: 'A code change that neither fixes a bug or adds a feature',
-      emoji: '💡',
-      value: 'refactor',
-    },
-    release: {
-      description: 'Create a release commit',
-      emoji: '🏹',
-      value: 'release',
-    },
-    style: {
-      description: 'Markup, white-space, formatting, missing semi-colons...',
-      emoji: '💄',
-      value: 'style',
-    },
-    test: {
-      description: 'Adding missing tests',
-      emoji: '💍',
-      value: 'test',
-    },
-  },
+  types: COMMIT_TYPES,
 };
