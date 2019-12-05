b1c326dd084c9a73401e34e76376d35ba9ba6bb2
Dico <didiraja@hotmail.com>
2019-12-05 17:27:46 -0300

    feat: 🎸 first version of getBrands()

--- packages/utils/index.js
+++ packages/utils/index.js
@@ -1,8 +1,10 @@
 import { timeout } from './ui.js';
 import * as Money from './money.js';
+import { getBrands } from './Brands/index.js';
 import { format as formatDate, parse as parseDate } from './date.js';
 
 export { timeout };
 export { Money };
 export { formatDate };
 export { parseDate };
+export { getBrands };
