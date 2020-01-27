142d82a143f90b73c8a72bc4759d621513e72759
Pedro Souza <souzapedro70@gmail.com>
2020-01-27 18:39:16 -0300

    feat: 🎸 Updated storage method

--- packages/pos/drivers/storage/wrappers.js
+++ packages/pos/drivers/storage/wrappers.js
@@ -1,14 +1,14 @@
 export default function(driver) {
-  driver.setItem = (key, value) => {
+  driver.setItem = (key, value, idAppStorage = -1) => {
     if (typeof value !== 'string') {
       value = JSON.stringify(value);
     }
-    driver.set(key, value);
+    driver.set(key, value, idAppStorage);
     return true;
   };
 
-  driver.getItem = key => {
-    const value = driver.get(key) || null;
+  driver.getItem = (key, idAppStorage = -1) => {
+    const value = driver.get(key, idAppStorage) || null;
     try {
       return JSON.parse(value);
     } catch (e) {
