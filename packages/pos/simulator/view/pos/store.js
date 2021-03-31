89a789b42b674e6df04803f1fe31e385acaae45b
Jonas Alves <joonasjss@gmail.com>
2021-03-31 17:57:35 -0300

    fix: 🐛 Fix pr

--- packages/pos/simulator/view/pos/store.js
+++ packages/pos/simulator/view/pos/store.js
@@ -1,7 +1,6 @@
 import { Store } from 'svelte/store.js';
 import { getPosModel } from '@mamba/utils';
 import { Registry } from '../../index.js';
-console.log(Registry.persistent.get().$Device.device);
 
 export const INITIAL_DATA = {
   ACTIVE_MODEL: Registry.persistent.get().$System.model,
@@ -9,7 +8,7 @@ export const INITIAL_DATA = {
 
 const store = new Store(INITIAL_DATA);
 
-store.setDevice = model => {
+store.setPosModel = model => {
   Registry.persistent.set(draft => {
     draft.$System.model = model;
   });
