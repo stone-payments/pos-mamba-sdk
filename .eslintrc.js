cc72f9ed35270cf2735d05639ab52c0efde64941
jaikme <jaime.mqu@gmail.com>
2021-04-12 13:59:01 -0300

    chore(configs): starter sdk eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,12 @@
-module.exports = {};
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    browser: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  extends: ['prettier'],
+};
