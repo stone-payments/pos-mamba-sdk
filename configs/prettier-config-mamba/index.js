043bcb651fb186b4f7db6369af2bc2f37fd3a71f
jaikme <jaime.mqu@gmail.com>
2021-04-12 19:01:51 -0300

    feat(configs): add svelte props

--- configs/prettier-config-mamba/index.js
+++ configs/prettier-config-mamba/index.js
@@ -13,6 +13,7 @@ const lineLength = 100;
 exports.lineLength = lineLength;
 
 module.exports = {
+  // Native prettier options
   jsxBracketSameLine: true,
   printWidth: lineLength,
   bracketSpacing: true,
@@ -20,4 +21,21 @@ module.exports = {
   trailingComma: 'all',
   proseWrap: 'always',
   semi: true,
+
+  /* Svelte options */
+
+  /* Sort order for scripts, markup, and styles */
+  svelteSortOrder: 'options-markup-scripts-styles',
+
+  /* More strict HTML syntax: self-closed tags, quotes in attributes */
+  svelteStrictMode: true,
+
+  /* Put the `>` of a multiline element on a new line */
+  svelteBracketNewLine: true,
+
+  /* 'Option to enable/disable component attribute shorthand if attribute name and expressions are same', */
+  svelteAllowShorthand: true,
+
+  /* 'Whether or not to indent the code inside <script> and <style> tags in Svelte files', */
+  svelteIndentScriptAndStyle: true,
 };
