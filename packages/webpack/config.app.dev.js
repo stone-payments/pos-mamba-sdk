627dbac7c2582ebf78e6175dd5cba8e86141456f
jaikme <jaime.mqu@gmail.com>
2021-04-29 22:18:54 -0300

    feat: 🎸 implement development log endpoint

--- packages/webpack/config.app.dev.js
+++ packages/webpack/config.app.dev.js
@@ -4,6 +4,7 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const { fromCwd } = require('quickenv');
+const bodyParser = require('body-parser');
 
 module.exports = merge(require('./config.app.js'), {
   devtool: 'eval-source-map',
@@ -32,5 +33,26 @@ module.exports = merge(require('./config.app.js'), {
     port: 8080,
     publicPath: 'http://localhost:8080/',
     hot: true,
+    before: app => {
+      app.use(bodyParser.json());
+      app.post('/POS_LOGGER', (req, res) => {
+        const { level, text } = req.body;
+        switch (level) {
+          case 'info':
+            console.log(`\x1B[35m${text}\x1B[0m`);
+            break;
+          case 'warn':
+            console.log(`\x1B[33m${text}\x1B[0m`);
+            break;
+          case 'error':
+            console.log(`\u001b[1;31m${text}\u001b[0m`);
+            break;
+          default:
+            console.log(text);
+            break;
+        }
+        res.sendStatus(200);
+      });
+    },
   },
 });
