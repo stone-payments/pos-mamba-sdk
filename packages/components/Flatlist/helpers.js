6d8136d885c3a49343270e758ff67469dee4e8eb
jaikme <jaime.mqu@gmail.com>
2021-05-03 17:21:18 -0300

    refactor: 💡 add element guard for getBoundingClientRect

--- packages/components/Flatlist/helpers.js
+++ packages/components/Flatlist/helpers.js
@@ -1,21 +1,28 @@
 const toggleActive = (items, index, action) => {
-
   const hasActive = items.find(el => el.element.get().isActive);
-  if(hasActive) {
+  if (hasActive) {
     hasActive.element.set({
-      isActive: false
-    })
+      isActive: false,
+    });
   }
 
-  const item = items[index].element
+  const item = items[index].element;
 
   item.set({
-    isActive: true
+    isActive: true,
   });
-}
+};
 
 const scrollTo = (yaxis, item) => {
-  const { top } = item.element.refs.element.getBoundingClientRect();
+  const { element } = item.element.refs;
+  if (!element) {
+    if (__DEV__) {
+      console.warn('Refresh the page to reset <FlatList /> references');
+    }
+    return;
+  }
+
+  const { top } = element.getBoundingClientRect();
   if (top + yaxis >= window.innerHeight) {
     window.scrollTo(0, top + yaxis);
   } else {
@@ -24,18 +31,16 @@ const scrollTo = (yaxis, item) => {
 };
 
 const getPosition = (index, event) => {
-  if(event === 'up') {
-    return index !== null && index > 0 ? index - 1 : index
+  if (event === 'up') {
+    return index !== null && index > 0 ? index - 1 : index;
   }
   return index !== null ? index + 1 : 0;
-
-}
+};
 
 export const selectRowItem = (nodeList, index, yaxis, event = 'down') => {
-
   const selectIndex = getPosition(index, event);
 
-  if(nodeList[selectIndex]) {
+  if (nodeList[selectIndex]) {
     scrollTo(yaxis, nodeList[selectIndex]);
     toggleActive(nodeList, selectIndex, 'up');
     return selectIndex;
