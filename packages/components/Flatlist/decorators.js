a4e74bf9532195080d6f0815d590d7b00e5df346
jaikme <jaime.mqu@gmail.com>
2021-05-05 17:36:02 -0300

    fix: 🐛 decorator overrides mechanism

--- packages/components/Flatlist/decorators.js
+++ packages/components/Flatlist/decorators.js
@@ -2,6 +2,10 @@ import Icon from '@mamba/icon';
 import { MODELS, getPosModel } from '@mamba/utils/index.js';
 import { neutral800 } from '@mamba/styles/colors.js';
 
+let ACTIVE_MODEL = getPosModel();
+let ACTIVE_MODEL_SLUG;
+let DefaultRowDecorator = {};
+
 const _getModelSlug = currentModel => {
   let val = 'S920';
   Object.keys(MODELS).forEach(key => {
@@ -12,16 +16,32 @@ const _getModelSlug = currentModel => {
   return val;
 };
 
-let ACTIVE_MODEL = getPosModel();
-let ACTIVE_MODEL_SLUG;
-let DefaultRowDecorator = {};
+const getModelSlug = model => _getModelSlug(model || ACTIVE_MODEL);
+
+const SEPARATORS = {
+  MP35P: '. ',
+  DEFAULT: ' - ',
+};
+
+const withPosPrefixSeparator = separator => position =>
+  (position && `${position}${separator}`) || null;
+
+const getPrefixOverride = (
+  overrides = {},
+  _default = {},
+  separator = SEPARATORS.DEFAULT,
+) => {
+  const keys = Object.getOwnPropertyNames(overrides);
+  const hasPrefix = keys.indexOf('prefix') !== -1;
+  const hasPrefixStyle = keys.indexOf('prefixStyle') !== -1;
+  const _defaultStyle = _default.prefixStyle || undefined;
+  return {
+    prefix: hasPrefix ? overrides.prefix : withPosPrefixSeparator(separator),
+    prefixStyle: hasPrefixStyle ? overrides.prefixStyle : _defaultStyle,
+  };
+};
 
 const SetDefaultRowDecorator = ({ label }, overrides = {}) => {
-  const defaultOptions = {
-    prefix: position => `${position}. `,
-    ...overrides,
-  };
-
   DefaultRowDecorator = {
     endFixture: {
       value: () => Icon,
@@ -29,17 +49,20 @@ const SetDefaultRowDecorator = ({ label }, overrides = {}) => {
     },
     label: {
       ...label,
-      ...defaultOptions,
+      ...getPrefixOverride(overrides),
     },
+    ...overrides,
   };
   return DefaultRowDecorator;
 };
 
-const GetDefaultDecorator = (rowProps, overrides) => {
+const GetDefaultDecorator = (rowProps, overrides = {}) => {
   ACTIVE_MODEL = getPosModel();
 
   const defaultProps = rowProps || { label: {} };
   const labelData = (defaultProps && defaultProps.label) || {};
+
+  const currentOverride = overrides[getModelSlug(ACTIVE_MODEL)] || overrides;
 
   return (
     {
@@ -52,18 +75,21 @@ const GetDefaultDecorator = (rowProps, overrides) => {
             ...labelData.style,
             fontWeight: '700',
           },
-          prefix: position => `${position}. `,
-          prefixStyle: {
-            color: '$green500',
-          },
+          ...getPrefixOverride(
+            currentOverride,
+            {
+              prefixStyle: {
+                color: '$green500',
+              },
+            },
+            SEPARATORS.MP35P,
+          ),
         },
-        ...overrides,
+        ...currentOverride,
       },
-    }[ACTIVE_MODEL] || SetDefaultRowDecorator(defaultProps, overrides)
+    }[ACTIVE_MODEL] || SetDefaultRowDecorator(defaultProps, currentOverride)
   );
 };
-
-const getModelSlug = () => _getModelSlug(ACTIVE_MODEL);
 
 export {
   GetDefaultDecorator,
