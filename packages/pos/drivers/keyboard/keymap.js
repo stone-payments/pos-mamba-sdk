46be163f61792118f7bc2509165d3b95fd33feb7
Pedro Souza <pedro.souza@stone.com.br>
2021-06-15 17:53:40 -0300

    fix: 🐛 export keymap

---
+++ packages/pos/drivers/keyboard/keymap.js
@@ -0,0 +1,22 @@
+/**
+ * KeyCode <-> KeyName dictionary
+ */
+export const KEYMAP = Object.freeze({
+  13: 'enter',
+  8: 'back',
+  27: 'close',
+  17: 'help',
+  16: 'shortcuts',
+  38: 'keyup',
+  40: 'keydown',
+  48: '0',
+  49: '1',
+  50: '2',
+  51: '3',
+  52: '4',
+  53: '5',
+  54: '6',
+  55: '7',
+  56: '8',
+  57: '9',
+});
--- packages/pos/drivers/keyboard/wrappers.js
+++ packages/pos/drivers/keyboard/wrappers.js
@@ -1,25 +1,4 @@
-/**
- * KeyCode <-> KeyName dictionary
- */
-const KEYMAP = Object.freeze({
-  13: 'enter',
-  8: 'back',
-  27: 'close',
-  17: 'help',
-  16: 'shortcuts',
-  38: 'keyup',
-  40: 'keydown',
-  48: '0',
-  49: '1',
-  50: '2',
-  51: '3',
-  52: '4',
-  53: '5',
-  54: '6',
-  55: '7',
-  56: '8',
-  57: '9',
-});
+import { KEYMAP } from './keymap.js';
 
 export default function(driver) {
   /**
