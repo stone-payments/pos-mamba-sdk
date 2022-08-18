415e9dc1a9c1850ded50fc3bd7c6801550b3fdee
jaikme <jaime.mqu@gmail.com>
2022-08-18 16:06:57 -0300

    feat: 🎸 add D199 of cli deploy

--- packages/cli/consts.js
+++ packages/cli/consts.js
@@ -1,5 +1,6 @@
 const S920 = 'S920';
 const Q92 = 'Q92';
+const D199 = 'D199';
 const MP35P = 'MP35P';
 const V240M = 'V240M';
 
@@ -7,8 +8,9 @@ module.exports = {
   MODELS: Object.freeze({
     S920,
     Q92,
+    D199,
     MP35P,
     V240M,
   }),
-  PLATFORMS: [S920, Q92, MP35P, V240M],
+  PLATFORMS: [S920, Q92, D199, MP35P, V240M],
 };
