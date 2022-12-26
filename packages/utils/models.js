e365188d186d34b1cfed628ecfd3f97397adbb4e
jaikme <jaime.mqu@gmail.com>
2022-12-26 12:41:52 -0300

    fix: 🐛 add has-keyboard-light class

--- packages/utils/models.js
+++ packages/utils/models.js
@@ -470,6 +470,7 @@ export function getDeviceCapabilitiesClassList() {
     hasPrinter() && 'has-printer',
     hasKeyboard() && 'has-keyboard',
     hasArrowNavigation() && 'has-arrow-navigation',
+    hasKeyboardLight() && 'has-keyboard-light',
     // NETWORK
     hasEthernet() && 'has-ethernet',
     hasWifi() && 'has-wifi',
