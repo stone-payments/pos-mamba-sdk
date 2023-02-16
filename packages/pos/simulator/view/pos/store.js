7293f3cd5613d37d1f9925f5e0125c944b241cab
jaikme <jaime.mqu@gmail.com>
2023-02-16 14:11:48 -0300

    fix: 🐛 (simulator) fix generic model slug

--- packages/pos/simulator/view/pos/store.js
+++ packages/pos/simulator/view/pos/store.js
@@ -3,7 +3,7 @@ import { AVAILABLE_SLUGS, MODELS } from '@mamba/utils';
 import { Registry } from '../../index.js';
 
 export const INITIAL_DATA = {
-  ACTIVE_MODEL: MODELS[__PLATFORM__] || Registry.persistent.get().$System.model,
+  ACTIVE_MODEL: MODELS[__PLATFORM__ || MODELS.S920] || Registry.persistent.get().$System.model,
 };
 
 const store = new Store(INITIAL_DATA);
