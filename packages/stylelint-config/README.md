51fea850d23ce05013acd55047debb759540bdd8
jaikme <jaime.mqu@gmail.com>
2023-05-22 16:01:41 -0300

    refactor: 💡 separate stylelint config to package and bring new changes

---
+++ packages/stylelint-config/README.md
@@ -0,0 +1,51 @@
+# Mamba Stylelint Config
+
+> A sharable [Stylelint](https://stylelint.io/) configuration that enforce CSS rules of [Mamba SDK](https://mambasdk-docs.stone.com.br) projects.
+
+## Install
+
+```
+$ npm install --save-dev @mamba/stylelint-config
+```
+
+
+1. Install locally:
+
+```bash
+npm install --save-dev @mamba/stylelint-config stylelint
+```
+2. Enable this configuration in your Stylelint configuration file (e.g. `.stylelintrc.js`):
+
+```js
+module.exports = {
+  extends: ' @mamba/stylelint-config',
+};
+```
+
+This shareable config expands to the following:
+
+```js
+/** @mamba/stylelint-config/index.js */
+{
+  extends: [
+    'stylelint-config-html/html',
+    'stylelint-config-html/svelte',
+    'stylelint-config-standard',
+    'stylelint-prettier',
+  ],
+  customSyntax: 'postcss-html',
+  rules: {
+    // ...
+  }
+}
+```
+
+### VSCode integration
+
+To work with [Visual Studio Code](https://code.visualstudio.com), you must install the
+[stylelint.vscode-stylelint](https://marketplace.visualstudio.com/items?itemName=stylelint.vscode-stylelint)
+plugin.
+
+## License
+
+Licensed under the [Apache 2.0 License](/LICENSE).
---
+++ packages/stylelint-config/index.js
@@ -0,0 +1,118 @@
+/**
+ * Copyright 2021 Stone Co.
+ *
+ * Licensed under Apache License - Version 2.0
+ * http://www.apache.org/licenses/
+ *
+ */
+
+'use strict';
+
+module.exports = {
+  extends: [
+    'stylelint-config-html/html',
+    'stylelint-config-html/svelte',
+    'stylelint-config-standard',
+    'stylelint-prettier',
+  ],
+  customSyntax: 'postcss-html',
+
+  /** Report when a unnecessary stylelint disable comment is written */
+  reportNeedlessDisables: true,
+  rules: {
+    /** Allow postcss @at-rules */
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['if', 'else', 'each', 'for', 'mixin', 'extend'],
+      },
+    ],
+
+    /** Prevent extra space between @at-rule and ; for readability */
+    'at-rule-semicolon-space-before': 'never',
+
+    /** Autoprefixer takes care of this for us */
+    'at-rule-no-vendor-prefix': true,
+    'property-no-vendor-prefix': null,
+    'media-feature-name-no-vendor-prefix': true,
+    'selector-no-vendor-prefix': true,
+    'value-no-vendor-prefix': true,
+
+    /** Allow comments to not have a empty line before them */
+    'comment-empty-line-before': null,
+
+    /** Prevent the usage of !important (bad practice) */
+    'declaration-no-important': true,
+
+    /** Don't enforce newline in multiline declarations */
+    'declaration-colon-newline-after': null,
+
+    /** Require quotes for recommended font-family names */
+    'font-family-name-quotes': 'always-unless-keyword',
+
+    /** Prevent confusing colors, use #hex, rgb() or rgba() only */
+    'function-blacklist': ['hsl', 'hsla'],
+    'function-url-no-scheme-relative': true,
+
+    /** Require quotes for url() */
+    'function-url-quotes': 'never',
+
+    /** Always require attribute selectors to have quotes (when relevant) */
+    'selector-attribute-quotes': 'always',
+
+    /** Rules for normalizing extra spaces  */
+    'media-feature-parentheses-space-inside': 'never',
+    'selector-list-comma-newline-before': 'never-multi-line',
+    'selector-list-comma-space-after': 'always-single-line',
+    'selector-list-comma-space-before': 'never-single-line',
+    'media-feature-range-operator-space-after': 'always',
+    'media-feature-range-operator-space-before': 'always',
+    'value-list-comma-newline-after': null,
+    'value-list-comma-newline-before': null,
+
+    /** Allow to specify a unit even for 'zero' values */
+    'length-zero-no-unit': null,
+
+    /** Disallow #id selectors (bad practice) */
+    'selector-max-id': 0,
+
+    /** Allow only one '*' universal selector per selector */
+    'selector-max-universal': 1,
+
+    /** Allow specifying a selector by its element type since we have scoped css */
+    'selector-no-qualifying-type': null,
+
+    /** Enforce a quote pattern */
+    'string-quotes': 'single',
+
+    /** Enforce lowercase for keyword values  */
+    'value-keyword-case': [
+      'lower',
+      {
+        ignoreProperties: '/font/',
+
+        /** Ignore svelte inline style values */
+        ignoreKeywords: '/{.*?}/',
+      },
+    ],
+
+    /** Limit the number of adjacent empty lines (Autofixable). */
+    'max-empty-lines': 2,
+
+    /** Svelte related */
+
+    /** Allow svelte :global() */
+    'selector-pseudo-class-no-unknown': null,
+
+    /**
+     * Since stylelint reads all 'ref:*' as the same selector,
+     * this prevents it from complaining when multiple 'ref:*'
+     * are present on the same css
+     */
+    'no-descending-specificity': null,
+    'no-duplicate-selectors': true,
+
+    /** Ignore URL notation for @import rules */
+    'import-notation': 'string',
+  },
+};
