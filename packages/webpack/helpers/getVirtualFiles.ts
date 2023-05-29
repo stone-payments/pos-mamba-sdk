6a43c1954c6b54446f84a84c44efc36d08adb0a4
jaikme <jaime.mqu@gmail.com>
2023-05-29 19:41:41 -0300

    refactor: 💡 remove relative start

--- packages/webpack/helpers/getVirtualFiles.ts
+++ packages/webpack/helpers/getVirtualFiles.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 import { IS_BROWSER } from '@mamba/configs/envModes.cjs';
 import { fromWorkingDir } from '@mamba/utils';
 
-const VIRTUAL_FILES = [`./index.${IS_BROWSER ? 'browser' : 'pos'}.ts`];
+const VIRTUAL_FILES = [`index.${IS_BROWSER ? 'browser' : 'pos'}.ts`];
 const getVirtualFiles = (): any =>
   VIRTUAL_FILES.reduce((acc, filepath) => {
     if (!existsSync(fromWorkingDir('src', filepath))) {
