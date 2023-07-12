e07ad3855e93bc74aa3c4798cbaaa2997c86bf91
jaikme <jaime.mqu@gmail.com>
2023-07-12 20:03:44 -0300

    fix(commitlint): 🐛 fix some errors on commit types

--- packages/commitlint/types.js
+++ packages/commitlint/types.js
@@ -1,51 +1,22 @@
+/* eslint-disable prettier/prettier */
+/* prettier-ignore */
+/* eslint max-len: ["error", { "code": 200 }] */
 module.exports = [
-  { value: 'feat', name: 'feat:     ✨  A new feature', emoji: '✨' },
-  { value: 'fix', name: 'fix:      🐛  A bug fix', emoji: '🐛' },
-  { value: 'docs', name: 'docs:     📝  Documentation only changes', emoji: '📝' },
-  {
-    value: 'style',
-    name: 'style:    💄  Changes that do not affect the meaning of the code',
-    emoji: ':lipstick:',
-  },
-  {
-    value: 'refactor',
-    name: 'refactor: ♻️   A code change that neither fixes a bug nor adds a feature',
-    emoji: '♻️',
-  },
-  { value: 'perf', name: 'perf:     ⚡️  A code change that improves performance', emoji: ':zap:' },
-  {
-    value: 'test',
-    name: 'test:     ✅  Adding missing tests or correcting existing tests',
-    emoji: ':white_check_mark:',
-  },
-  {
-    value: 'build',
-    name: 'build:    📦️  Changes that affect the build system or external dependencies',
-    emoji: ':package:',
-  },
-  {
-    value: 'ci',
-    name: 'ci:       🎡  Changes to our CI configuration files and scripts',
-    emoji: ':ferris_wheel:',
-  },
-  {
-    value: 'chore',
-    name: "chore:    🛠   Other changes that don't modify src or test files",
-    emoji: ':hammer:',
-  },
-  { value: 'revert', name: 'revert:   ⏪️  Reverts a previous commit', emoji: ':rewind:' },
-  { value: 'release', name: 'release:  🏹  Create a release commit', emoji: ':bow_and_arrow:' },
-  { value: 'fire', name: 'fire:     🔥  Removing code or files', emoji: ':fire:' },
-  {
-    value: 'conf',
-    name: 'conf:     🏗   A commit related to configs or scaffolding',
-    emoji: ':gear:',
-  },
-  { value: 'shirt', name: 'shirt:    👔  Removing linter warnings', emoji: ':shirt:' },
-  { value: 'wip', name: 'wip:      🚧  A work in progress commit', emoji: ':construction:' },
-  {
-    value: 'security',
-    name: 'security: 🔒  A commit that improves or fixes security',
-    emoji: ':lock:',
-  },
+  { value: 'feat',     name: 'feat:     ✨  A new feature',                                                 emoji: '✨' },
+  { value: 'fix',      name: 'fix:      🐛  A bug fix',                                                     emoji: '🐛' },
+  { value: 'docs',     name: 'docs:     📝  Documentation only changes',                                    emoji: '📝' },
+  { value: 'style',    name: 'style:    💄  Changes that do not affect the meaning of the code',            emoji: '💄' },
+  { value: 'refactor', name: 'refactor: ♻️   A code change that neither fixes a bug nor adds a feature',     emoji: '♻️' },
+  { value: 'perf',     name: 'perf:     ⚡️  A code change that improves performance',                        emoji: '⚡️' },
+  { value: 'test',     name: 'test:     ✅  Adding missing tests or correcting existing tests',             emoji: '✅' },
+  { value: 'build',    name: 'build:    📦️  Changes that affect the build system or external dependencies', emoji: '📦️' },
+  { value: 'ci',       name: 'ci:       🎡  Changes to our CI configuration files and scripts',             emoji: '🎡' },
+  { value: 'chore',    name: "chore:    🛠   Other changes that don't modify src or test files",            emoji: '🛠' },
+  { value: 'revert',   name: 'revert:   ⏪️  Reverts a previous commit',                                     emoji: '⏪️' },
+  { value: 'release',  name: 'release:  🏹  Create a release commit',                                       emoji: '🏹' },
+  { value: 'fire',     name: 'fire:     🔥  Removing code or files',                                        emoji: '🔥' },
+  { value: 'conf',     name: 'conf:     🏗   A commit related to configs or scaffolding',                   emoji: '🏗' },
+  { value: 'shirt',    name: 'shirt:    👔  Removing linter warnings',                                      emoji: '🚧' },
+  { value: 'wip',      name: 'wip:      🚧  A work in progress commit',                                     emoji: '👔' },
+  { value: 'security', name: 'security: 🔒  A commit that improves or fixes security',                      emoji: '🔒' },
 ];
