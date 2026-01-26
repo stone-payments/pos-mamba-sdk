e01cbb9775b5c741b705bde3c1b89e411b49795f
Daniel Dias <ddias@stone.com.br>
2026-01-26 10:09:45 -0300

    Fix repo_setup.py on gitignore

--- tools/repo_initialization.sh
+++ tools/repo_initialization.sh
@@ -66,6 +66,7 @@ function download_from_tools_on_mamba_sdk() {
 
   local FILENAME=$(basename "$FILE_TO_DOWNLOAD")
   local OUTPUT_PATH="$DOWNLOAD_TO/$FILENAME"
+  OUTPUT_PATH="${OUTPUT_PATH#./}"  # Remove ./ prefix if present
 
   if [ -n "$GITHUB_TOKEN" ]; then
     curl -GLf \
